a1ef979a00558b05b4809d6f2a512d64856932c9
ysdy44 <ysdy44@outlook.com>
2023-06-12 22:44:20 +0800

    Add MainPage.UI Click

---
+++ Virtual Piano/MainPage.Click.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Virtual_Piano.Notes;
+
+namespace Virtual_Piano
+{
+    partial class MainPage
+    {
+
+        //@Delegate
+        public event EventHandler CanExecuteChanged;
+
+        //@Command
+        public ICommand Command => null;
+        public bool CanExecute(object parameter) => parameter != default;
+        public void Execute(object parameter)
+        {
+        }
+    }
+}
---
+++ Virtual Piano/MainPage.UI.cs
@@ -0,0 +1,28 @@
+﻿using Virtual_Piano.Notes;
+
+namespace Virtual_Piano
+{
+    partial class MainPage
+    {
+        public void Clear(int item)
+        {
+        }
+        public void Add(int item)
+        {
+        }
+
+        public void Clear(Note item)
+        {
+        }
+        public void Add(Note item)
+        {
+        }
+
+        public void Clear(MidiPercussionNote item)
+        {
+        }
+        public void Add(MidiPercussionNote item)
+        {
+        }
+    }
+}
