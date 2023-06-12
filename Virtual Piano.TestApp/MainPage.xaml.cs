f8a9296a0e7d329d598fb3cf042608ef41f79932
ysdy44 <ysdy44@outlook.com>
2023-06-12 22:21:45 +0800

    添加项目文件。

---
+++ Virtual Piano.TestApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Virtual Piano/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
