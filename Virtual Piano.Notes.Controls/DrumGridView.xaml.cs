bacad0787d7fc8422c63f9cc78cddc5cf49bedea
ysdy44 <ysdy44@outlook.com>
2023-06-18 20:10:06 +0800

    Add DrumGridView

---
+++ Virtual Piano.Notes.Controls/DrumGridView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public partial class DrumGridView : UserControl
+    {
+        //@Command
+        public ICommand Command { get; set; }
+
+        public DrumGridView()
+        {
+            this.InitializeComponent();
+            this.GridView.ItemsSource = this.GetItemSource().ToArray();
+            this.GridView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is DrumItem item)
+                {
+                    this.Command?.Execute(item.Note); // Command
+                }
+            };
+        }
+
+        public virtual string GetString(MidiPercussionNote note)
+        {
+            return note.ToString();
+        }
+
+        private IEnumerable<DrumItem> GetItemSource()
+        {
+            foreach (MidiPercussionNote item in System.Enum.GetValues(typeof(MidiPercussionNote)))
+            {
+                yield return new DrumItem
+                {
+                    Note = item,
+                    Text = this.GetString(item)
+                };
+            }
+        }
+    }
+}
---
+++ Virtual Piano.Notes.Controls/DrumGroupGridView.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public partial class DrumGroupGridView : UserControl
+    {
+        //@Command
+        public ICommand Command { get; set; }
+
+        public DrumItemCategory[] ItemsSource { get; }
+        public IList<MidiPercussionNoteCategory> Collection { get; } = new List<MidiPercussionNoteCategory>();
+
+        public IDictionary<MidiPercussionNoteCategory, DrumItem[]> Dictionary { get; } = new Dictionary<MidiPercussionNoteCategory, DrumItem[]>();
+        public IList<DrumItem> ObservableCollection { get; } = new ObservableCollection<DrumItem>();
+
+        public DrumGroupGridView()
+        {
+            this.InitializeComponent();
+            this.ItemsSource = MidiPercussionNoteFactory.Instance.Select(c => new DrumItemCategory
+            {
+                Category = c.Key,
+                Text = this.GetString(c.Key),
+            }).ToArray();
+
+            foreach (var item in MidiPercussionNoteFactory.Instance)
+            {
+                this.Dictionary[item.Key] = item.Value.Select(c => new DrumItem
+                {
+                    Note = c,
+                    Text = this.GetString(c),
+                }).ToArray();
+            }
+
+            this.ListView.SelectionChanged += (s, e) =>
+            {
+                foreach (DrumItemCategory item in e.RemovedItems)
+                {
+                    this.Collection.Remove(item.Category);
+                    foreach (var item2 in this.Dictionary[item.Category])
+                    {
+                        this.ObservableCollection.Remove(item2);
+                    }
+                }
+                foreach (DrumItemCategory item in e.AddedItems)
+                {
+                    this.Collection.Add(item.Category);
+                    foreach (var item2 in this.Dictionary[item.Category])
+                    {
+                        this.ObservableCollection.Add(item2);
+                    }
+                }
+            };
+            this.GridView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is DrumItem item)
+                {
+                    this.Command?.Execute(item.Note); // Command
+                }
+            };
+        }
+
+        public virtual string GetString(MidiPercussionNote note)
+        {
+            return note.ToString();
+        }
+        public virtual string GetString(MidiPercussionNoteCategory category)
+        {
+            return category.ToString();
+        }
+    }
+}
