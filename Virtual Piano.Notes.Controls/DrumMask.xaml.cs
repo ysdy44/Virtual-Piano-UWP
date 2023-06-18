1113c5ea0e749b07639469f3a93a3f316b7987d6
ysdy44 <ysdy44@outlook.com>
2023-06-18 19:58:43 +0800

    Add DrumMask

---
+++ Virtual Piano.Notes.Controls/DrumMask.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public sealed partial class DrumMask : UserControl
+    {
+        public DrumMask()
+        {
+            this.InitializeComponent();
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+
+                int w = (int)e.NewSize.Width;
+                int h = (int)e.NewSize.Height;
+                int min = System.Math.Min(w, h);
+
+                this.Shape1.Width = w;
+                this.Shape1.Height = h;
+
+                int s2 = min * 5 / 12;
+                this.Shape2.Width =
+                this.Shape2.Height = s2 + s2;
+                Canvas.SetLeft(this.Shape2, w / 2 - s2);
+                Canvas.SetTop(this.Shape2, h / 2 - s2);
+
+                int s3 = min * 3 / 12;
+                this.Shape3.Width =
+                this.Shape3.Height = s3 + s3;
+                Canvas.SetLeft(this.Shape3, w / 2 - s3);
+                Canvas.SetTop(this.Shape3, h / 2 - s3);
+            };
+        }
+    }
+}
