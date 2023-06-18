5ebb3980386c54d9b95ad8534e584d6a60fee038
ysdy44 <ysdy44@outlook.com>
2023-06-18 20:12:19 +0800

    UI Add Drums

---
+++ Virtual Piano/Controls/DrumGridView.cs
@@ -0,0 +1,12 @@
+﻿using Virtual_Piano.Notes;
+
+namespace Virtual_Piano.Controls
+{
+    public class DrumGridView : Notes.Controls.DrumGridView
+    {
+        public override string GetString(MidiPercussionNote note)
+        {
+            return App.Resource.GetString(note.ToString());
+        }
+    }
+}
---
+++ Virtual Piano/Controls/DrumGroupGridView.cs
@@ -0,0 +1,16 @@
+﻿using Virtual_Piano.Notes;
+
+namespace Virtual_Piano.Controls
+{
+    public class DrumGroupGridView : Notes.Controls.DrumGroupGridView
+    {
+        public override string GetString(MidiPercussionNote note)
+        {
+            return App.Resource.GetString(note.ToString());
+        }
+        public override string GetString(MidiPercussionNoteCategory category)
+        {
+            return App.Resource.GetString(category.ToString());
+        }
+    }
+}
