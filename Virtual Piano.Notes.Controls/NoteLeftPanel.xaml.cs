97263b00ef5fedf1293adf6376046d087e74b50c
ysdy44 <ysdy44@outlook.com>
2023-06-22 23:42:40 +0800

    Add NoteLeftPanel

---
+++ Virtual Piano.Notes.Controls/NoteLeftPanel.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public sealed partial class NoteLeftPanel : NotePanel
+    {
+        public NoteLeftPanel() : base(NoteDirection.Left)
+        {
+            this.InitializeComponent();
+            base.Initialize();
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                base.Resize(e.NewSize, e.PreviousSize);
+            };
+        }
+        public override Brush GetBrush(Octave octave) => null;
+        public override Style GetStyle(ToneType type) => this.Resources[$"{type}"] as Style;
+    }
+}
