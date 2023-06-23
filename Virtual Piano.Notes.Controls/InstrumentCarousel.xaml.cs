93007f7c30a46783d44946cf884cc0bc1dfacca5
ysdy44 <ysdy44@outlook.com>
2023-06-23 19:49:49 +0800

    Add InstrumentCarousel

--- Virtual Piano.Notes.Controls/InstrumentCarousel.xaml.cs
+++ Virtual Piano.Notes.Controls/InstrumentCarousel.xaml.cs
@@ -1,23 +1,7 @@
 ﻿using System.Linq;
 using System.Windows.Input;
-using Virtual_Piano.Notes;
 using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
-
-namespace Virtual_Piano.Controls
-{
-    public class InstrumentCarousel : Notes.Controls.InstrumentCarousel
-    {
-        public override string GetString(MidiProgram note)
-        {
-            return App.Resource.GetString(note.ToString());
-        }
-        public override string GetString(MidiProgramGroup group)
-        {
-            return App.Resource.GetString(group.ToString());
-        }
-    }
-}
 
 namespace Virtual_Piano.Notes.Controls
 {
---
+++ Virtual Piano/Controls/InstrumentCarousel.cs
@@ -0,0 +1,16 @@
+﻿using Virtual_Piano.Notes;
+
+namespace Virtual_Piano.Controls
+{
+    public class InstrumentCarousel : Notes.Controls.InstrumentCarousel
+    {
+        public override string GetString(MidiProgram note)
+        {
+            return App.Resource.GetString(note.ToString());
+        }
+        public override string GetString(MidiProgramGroup group)
+        {
+            return App.Resource.GetString(group.ToString());
+        }
+    }
+}
