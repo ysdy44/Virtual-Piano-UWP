44ec048d6903cbc8b0c734344e05c6c403fd8a99
ysdy44 <ysdy44@outlook.com>
2023-06-23 15:43:08 +0800

    Add NoteHorizontalScrollViewer

---
+++ Virtual Piano.Notes.Controls/Interfaces/INoteScrollViewer.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public interface INoteScrollViewer
+    {
+        int ItemSize { get; set; }
+        ScrollViewer ScrollViewer { get; set; }
+
+        void PageDown();
+        void PageUp();
+    }
+}
---
+++ Virtual Piano.Notes.Controls/NoteHorizontalScrollViewer.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public sealed partial class NoteHorizontalScrollViewer : UserControl, INoteScrollViewer
+    {
+
+        private NoteSize NoteSize = new NoteSize(20);
+        private double Length = NoteExtensions.NoteWhiteCount * 20 * NoteExtensions.ToneCount / NoteExtensions.WhiteCount;
+        public int ItemSize
+        {
+            get => this.NoteSize.ItemSize;
+            set
+            {
+                if (this.NoteSize.ItemSize == value) return;
+                this.NoteSize = new NoteSize(value);
+                this.Length = NoteExtensions.NoteWhiteCount * this.NoteSize.WhiteSize;
+
+                this.Rectangle.Width = base.ActualWidth * this.RootGrid.ActualWidth / this.Length;
+
+                if (this.ScrollViewer is null) return;
+                Canvas.SetLeft(this.Rectangle, this.ScrollViewer.HorizontalOffset * this.RootGrid.ActualWidth / this.Length);
+            }
+        }
+
+        private ScrollViewer scrollViewer;
+        public ScrollViewer ScrollViewer
+        {
+            get => this.scrollViewer;
+            set
+            {
+                if (this.scrollViewer != null) this.scrollViewer.ViewChanging -= this.ViewChanging;
+                this.scrollViewer = value;
+                if (this.scrollViewer != null) this.scrollViewer.ViewChanging += this.ViewChanging;
+            }
+        }
+
+        double Offset;
+
+        public NoteHorizontalScrollViewer()
+        {
+            this.InitializeComponent();
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                if (e.NewSize.Width == e.PreviousSize.Width) return;
+
+                this.Rectangle.Width = e.NewSize.Width * this.RootGrid.ActualWidth / this.Length;
+            };
+            this.RootGrid.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                if (e.NewSize.Width == e.PreviousSize.Width) return;
+
+                this.Rectangle.Width = base.ActualWidth * e.NewSize.Width / this.Length;
+
+                if (this.ScrollViewer is null) return;
+                Canvas.SetLeft(this.Rectangle, this.ScrollViewer.HorizontalOffset * e.NewSize.Width / this.Length);
+            };
+
+            this.PreviousButton.Click += (s, e) => this.PageUp();
+            this.NextButton.Click += (s, e) => this.PageDown();
+
+            this.Thumb.DragStarted += (s, e) =>
+            {
+                this.Offset = e.HorizontalOffset - this.Rectangle.ActualWidth / 2;
+                this.Offset = this.Offset * this.Length / this.RootGrid.ActualWidth;
+                this.ScrollViewer.ChangeView(this.Offset, null, null, true);
+            };
+            this.Thumb.DragDelta += (s, e) =>
+            {
+                this.Offset += e.HorizontalChange * this.Length / this.RootGrid.ActualWidth;
+                this.ScrollViewer.ChangeView(this.Offset, null, null, true);
+            };
+            this.Thumb.DragCompleted += (s, e) =>
+            {
+                this.ScrollViewer.ChangeView(this.Offset, null, null, true);
+            };
+        }
+
+        private void ViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
+        {
+            Canvas.SetLeft(this.Rectangle, e.NextView.HorizontalOffset * this.RootGrid.ActualWidth / this.Length);
+        }
+
+        public void PageUp()
+        {
+            if (this.ScrollViewer is null) return;
+            this.Offset = this.ScrollViewer.HorizontalOffset;
+            this.Offset -= this.NoteSize.WhiteSize;
+            this.ScrollViewer.ChangeView(this.Offset, null, null, false);
+        }
+        public void PageDown()
+        {
+            if (this.ScrollViewer is null) return;
+            this.Offset = this.ScrollViewer.HorizontalOffset;
+            this.Offset += this.NoteSize.WhiteSize;
+            this.ScrollViewer.ChangeView(this.Offset, null, null, false);
+        }
+    }
+}
