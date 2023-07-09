daaeefc35ef5db2a051cd2ab0bf4ba5805969266
ysdy44 <ysdy44@outlook.com>
2023-07-09 17:43:51 +0800

    TestApp Add CompositionPage

---
+++ Virtual Piano.TestApp/CompositionPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Virtual_Piano.Elements;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class CompositionPage : Page
+    {
+        public CompositionPage()
+        {
+            this.InitializeComponent();
+            var m = this.ScrollViewer.GetManipulation();
+            var x = m.ExpressionX();
+            var y = m.ExpressionY();
+            this.Left.GetVisual().OffsetX(x);
+            this.Top.GetVisual().OffsetY(y);
+            var v = this.LeftTop.GetVisual();
+            v.OffsetX(x);
+            v.OffsetY(y);
+        }
+    }
+}
