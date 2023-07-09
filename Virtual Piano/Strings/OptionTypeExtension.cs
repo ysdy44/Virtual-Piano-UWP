08a18d48e8ec385b5362606f9b634bf9b50effbe
ysdy44 <ysdy44@outlook.com>
2023-07-09 20:00:37 +0800

    Add OptionExtension and OptionTypeExtension

---
+++ Virtual Piano/Strings/OptionTypeExtension.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Markup;
+
+namespace Virtual_Piano.Strings
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(OptionType))]
+    public class OptionTypeExtension : MarkupExtension
+    {
+        public OptionType Type { get; set; }
+        protected override object ProvideValue() => this.Type;
+    }
+}
