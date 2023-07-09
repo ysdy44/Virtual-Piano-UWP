8d7aea11f0947840824220b6513e87e0c9596f14
ysdy44 <ysdy44@outlook.com>
2023-07-09 18:35:14 +0800

    Add MidiNotePage

---
+++ Virtual Piano.TestApp/MidiNotePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Windows.Devices.Midi;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class MidiNotePage : Page
+    {
+        readonly int[] Items = System.Linq.Enumerable.Range(0, 15).ToArray();
+        readonly int[] ItemsSource = System.Linq.Enumerable.Range(0, 128).ToArray();
+
+        public MidiSynthesizer Synthesizer { get; set; }
+
+        public MidiNotePage()
+        {
+            this.InitializeComponent();
+            this.GridView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is int n)
+                {
+                    int index = ListView.SelectedIndex;
+                    if (index < 0) index = 0;
+                    this.Synthesizer?.SendMessage(new MidiNoteOnMessage((byte)index, (byte)n, 127));
+                }
+            };
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.Synthesizer?.Dispose();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Synthesizer?.Dispose();
+            this.Synthesizer = await MidiSynthesizer.CreateAsync();
+        }
+    }
+}
