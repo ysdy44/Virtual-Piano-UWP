65f2bf7cb6c98720d3e14a0e5466467b2d80c25b
ysdy44 <ysdy44@outlook.com>
2023-07-09 18:07:16 +0800

    Add MidiPercussionPage

---
+++ Virtual Piano.TestApp/MidiPercussionPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Virtual_Piano.Notes;
+using Windows.Devices.Midi;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Virtual_Piano.TestApp
+{
+    internal sealed class PercussionGrouping : List<MidiPercussionNote>, IList<MidiPercussionNote>, IGrouping<MidiPercussionNoteCategory, MidiPercussionNote>
+    {
+        public MidiPercussionNoteCategory Key { get; }
+        public PercussionGrouping(MidiPercussionNoteCategory key, IEnumerable<MidiPercussionNote> collection) : base(collection) => this.Key = key;
+        public override string ToString() => this.Key.ToString();
+    }
+
+    public sealed partial class MidiPercussionPage : Page
+    {
+        private PercussionGrouping[] Groupings =>
+            MidiPercussionNoteFactory.Instance.Select(c =>
+            new PercussionGrouping(c.Key, c.Value)).ToArray();
+
+        readonly byte Channel = 9;
+
+        byte Note;
+        public MidiSynthesizer Synthesizer { get; set; }
+
+        public MidiPercussionPage()
+        {
+            this.InitializeComponent();
+            this.ListView.ItemClick += (s, e) =>
+            {
+                if (e.ClickedItem is MidiPercussionNote item)
+                {
+                    byte n = (byte)item;
+                    this.Synthesizer?.SendMessage(new MidiNoteOffMessage(this.Channel, this.Note, 127));
+                    this.Synthesizer?.SendMessage(new MidiNoteOnMessage(this.Channel, n, 127));
+                    this.Note = (byte)n;
+                }
+            };
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.Synthesizer?.Dispose();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Synthesizer?.Dispose();
+            this.Synthesizer = await MidiSynthesizer.CreateAsync();
+        }
+    }
+}
