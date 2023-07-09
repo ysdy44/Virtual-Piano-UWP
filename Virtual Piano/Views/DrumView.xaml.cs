b3b2da7e165b07f7d140b10c5330d2c24f82becb
ysdy44 <ysdy44@outlook.com>
2023-07-09 19:50:31 +0800

    Add KeyboardView, DrumView and ChordView

---
+++ Virtual Piano/Views/DrumView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Virtual_Piano.Views
+{
+    public sealed partial class DrumView : Page
+    {
+        //@Converter
+        private Visibility BooleanToVisibilityConverter(bool value) => value ? Visibility.Visible : Visibility.Collapsed;
+
+        ICommand Command;
+
+        public DrumView()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.Command = null;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Command = e.Parameter as ICommand;
+        }
+    }
+}
