15ad12a0c5174e07b4ffd52e0ea380563575b098
ysdy44 <ysdy44@outlook.com>
2023-07-11 01:05:31 +0800

    Add VelocitySlider

---
+++ Virtual Piano.Notes.Controls/Velocitys/VelocitySlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using Windows.Devices.Midi;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public sealed class VelocitySlider : Slider
+    {
+        //@Command
+        public ICommand Command { get; set; }
+        public ControlController Controller { get; set; }
+
+        public VelocitySlider()
+        {
+            base.ValueChanged += (s, e) =>
+            {
+                int value = System.Math.Clamp((int)e.NewValue, 0, VelocityGaugeBase.Velocity);
+                this.Command?.Execute(new Message
+                {
+                    Type = MidiMessageType.ControlChange,
+                    Controller = this.Controller,
+                    ControllerValue = (byte)value
+                }); // Command
+            };
+        }
+    }
+}
