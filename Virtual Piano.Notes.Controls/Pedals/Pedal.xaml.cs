0995ac270550ea502f7f7ce6010164470b6bfafd
ysdy44 <ysdy44@outlook.com>
2023-07-11 02:21:55 +0800

    Add Pedal

---
+++ Virtual Piano.Notes.Controls/Pedals/Pedal.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+using Windows.Devices.Midi;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public sealed partial class Pedal : ContactButton
+    {
+        //@Command
+        public ICommand Command { get; set; }
+        public ControlController Controller { get; set; }
+
+        public Pedal()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnContactOn()
+        {
+            this.Command?.Execute(new Message
+            {
+                Type = MidiMessageType.ControlChange,
+                Controller = this.Controller,
+                ControllerValue = 127
+            }); // Command
+        }
+
+        protected override void OnContactOff()
+        {
+            this.Command?.Execute(new Message
+            {
+                Type = MidiMessageType.ControlChange,
+                Controller = this.Controller,
+                ControllerValue = 0
+            }); // Command
+        }
+    }
+}
