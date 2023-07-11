3d496b804fe9f0ad9af2a7e4f5fdb29956fbe01e
ysdy44 <ysdy44@outlook.com>
2023-07-11 21:17:59 +0800

    Add ColorPage

---
+++ Virtual Piano.TestApp/ColorPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class ColorPage : Page
+    {
+        public ColorPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Virtual Piano.TestApp/ColorView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class ColorView : UserControl
+    {
+        public ColorView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
