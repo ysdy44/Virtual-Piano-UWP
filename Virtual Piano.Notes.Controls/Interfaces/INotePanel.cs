e14cc6a010966467773b8bdaf48d2ffa03c7d4dd
ysdy44 <ysdy44@outlook.com>
2023-07-14 13:53:27 +0800

    File rename: Note -> Piano

--- Virtual Piano.Notes.Controls/Interfaces/INotePanel.cs
+++ Virtual Piano.Notes.Controls/Interfaces/INotePanel.cs
@@ -8,19 +8,4 @@ namespace Virtual_Piano.Notes.Controls
 
         void OnClick(Note note);
     }
-    public interface IPianoPanel : INotePanel
-    {
-        double WhiteSize { get; set; }
-        double BlackSize { get; set; }
-
-        int ItemSize { get; set; }
-        KeyLabel Label { get; set; }
-
-        INoteButton this[Note item] { get; }
-
-        void Clear(int index);
-        void Add(int index);
-        void Clear(Note note);
-        void Add(Note note);
-    }
 }
---
+++ Virtual Piano.Notes.Controls/Interfaces/IPianoPanel.cs
@@ -0,0 +1,18 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public interface IPianoPanel : INotePanel
+    {
+        double WhiteSize { get; set; }
+        double BlackSize { get; set; }
+
+        int ItemSize { get; set; }
+        KeyLabel Label { get; set; }
+
+        INoteButton this[Note item] { get; }
+
+        void Clear(int index);
+        void Add(int index);
+        void Clear(Note note);
+        void Add(Note note);
+    }
+}
