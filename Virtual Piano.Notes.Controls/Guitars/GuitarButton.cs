2fd60c1ec2fad9d18f4702154b0436937156165b
ysdy44 <ysdy44@outlook.com>
2023-07-15 00:18:09 +0800

    Add GuitarButton

---
+++ Virtual Piano.Notes.Controls/Guitars/GuitarButton.cs
@@ -0,0 +1,17 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public sealed class GuitarButton : ClickButton, IGuitarButton
+    {
+        //@Command
+        public Note CommandParameter { get; set; }
+        public GuitarString Strings { get; set; }
+
+        protected override void OnClick()
+        {
+            if (base.Parent is IGuitarPanel item)
+            {
+                item.OnClick(this.CommandParameter, this.Strings);
+            }
+        }
+    }
+}
