6d39e1db580673f910360e223c85eb08e071bc30
ysdy44 <ysdy44@outlook.com>
2023-07-16 14:54:51 +0800

    Add CommandCheckedButton

---
+++ Virtual Piano.Elements/CommandCheckedButton.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Elements
+{
+    public class CommandCheckedButton : Button
+    {
+        public Symbol CheckedSymbol { get; set; }
+        public Symbol UncheckedSymbol { get; set; }
+
+        public Symbol Symbol
+        {
+            get => this.SymbolIcon.Symbol;
+            private set => this.SymbolIcon.Symbol = value;
+        }
+
+        public bool IsChecked
+        {
+            get => this.isChecked;
+            set
+            {
+                this.isChecked = value;
+                this.Symbol = value ? this.CheckedSymbol : this.UncheckedSymbol;
+            }
+        }
+        private bool isChecked;
+
+        readonly SymbolIcon SymbolIcon = new SymbolIcon();
+
+        //@Construct
+        public CommandCheckedButton()
+        {
+            base.Content = this.SymbolIcon;
+            base.Loaded += (s, e) => this.Symbol = this.IsChecked ? this.CheckedSymbol : this.UncheckedSymbol;
+        }
+    }
+}
--- Virtual Piano.Elements/CommandToggleButton.cs
+++ Virtual Piano.Elements/CommandToggleButton.cs
@@ -1,52 +1,24 @@
-﻿using Windows.UI.Xaml.Controls;
-
-namespace Virtual_Piano.Elements
+﻿namespace Virtual_Piano.Elements
 {
-    public sealed class CommandToggleButton : Button
+    public  class CommandToggleButton : CommandCheckedButton
     {
         //@Command
         public object UncheckedCommandParameter { get; set; }
         public object CheckedCommandParameter { get; set; }
 
-        public Symbol CheckedSymbol { get; set; }
-        public Symbol UncheckedSymbol { get; set; }
-
-        public bool IsChecked
-        {
-            get => this.isChecked;
-            set
-            {
-                this.isChecked = value;
-                this.Symbol = value ? this.CheckedSymbol : this.UncheckedSymbol;
-            }
-        }
-        private bool isChecked;
-
-        public Symbol Symbol
-        {
-            get => this.SymbolIcon.Symbol;
-            private set => this.SymbolIcon.Symbol = value;
-        }
-
-        readonly SymbolIcon SymbolIcon = new SymbolIcon();
-
         //@Construct
         public CommandToggleButton()
         {
-            base.Content = this.SymbolIcon;
-            base.Loaded += (s, e) => this.Symbol = this.IsChecked ? this.CheckedSymbol : this.UncheckedSymbol;
             base.Click += (s, e) =>
             {
-                if (this.isChecked)
+                if (this.IsChecked)
                 {
-                    this.isChecked = false;
-                    this.Symbol = this.UncheckedSymbol;
+                    this.IsChecked = false;
                     base.Command?.Execute(this.UncheckedCommandParameter); // Command
                 }
                 else
                 {
-                    this.isChecked = true;
-                    this.SymbolIcon.Symbol = this.CheckedSymbol;
+                    this.IsChecked = true;
                     base.Command?.Execute(this.CheckedCommandParameter); // Command
                 }
             };
