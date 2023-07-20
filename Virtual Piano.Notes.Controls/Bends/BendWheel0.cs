713914db295006aa398eed5fafef36e6ef524bf2
ysdy44 <ysdy44@outlook.com>
2023-07-20 13:24:16 +0800

    File Add BendWheel0 and BendWheel64

---
+++ Virtual Piano.Notes.Controls/Bends/BendWheel0.cs
@@ -0,0 +1,10 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public abstract class BendWheel0 : BendWheel
+    {
+        protected BendWheel0() : base(0) { }
+
+        public override void Start() { }
+        public override void Stop() { }
+    }
+}
