d6bab3e500425cb5c17c2bf9c34ee6008cf02c18
ysdy44 <ysdy44@outlook.com>
2023-07-23 03:55:52 +0800

    Add IKitPanel and IKitButton

---
+++ Virtual Piano.Notes.Controls/Interfaces/IKitButton.cs
@@ -0,0 +1,7 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public interface IKitButton : IClickButton
+    {
+        KitSet CommandParameter { get; set; }
+    }
+}
---
+++ Virtual Piano.Notes.Controls/Interfaces/IKitPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public interface IKitPanel
+    {
+        ICommand Command { get; set; }
+
+        void OnClick(KitSet set);
+    }
+}
