5ed92fe187e0f0bfae52b6a28cf62063aa94d545
ysdy44 <ysdy44@outlook.com>
2023-07-23 03:57:29 +0800

    Add KitPanel and KitButton

---
+++ Virtual Piano.Notes.Controls/Drums/KitButton.cs
@@ -0,0 +1,16 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public sealed class KitButton : ClickButton, IKitButton
+    {
+        //@Command
+        public KitSet CommandParameter { get; set; }
+
+        protected override void OnClick()
+        {
+            if (base.Parent is IKitPanel item)
+            {
+                item.OnClick(this.CommandParameter);
+            }
+        }
+    }
+}
---
+++ Virtual Piano.Notes.Controls/Drums/KitPanel.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Notes.Controls
+{
+    public partial class KitPanel : Canvas, IKitPanel
+    {
+        //@Command
+        public ICommand Command { get; set; }
+
+        readonly IList<KitSet> ItemsSource = new List<KitSet>
+        {
+            KitSet.Crash,
+            KitSet.Ride,
+            KitSet.Open,
+            KitSet.Close,
+            KitSet.Pedal,
+            KitSet.HiTom,
+            KitSet.LowTom,
+            KitSet.FloorTom,
+            KitSet.Snare,
+            KitSet.Stick,
+            KitSet.Kick,
+        };
+
+        public KitPanel()
+        {
+            this.InitializeComponent();
+        }
+
+        public void OnClick(KitSet set)
+        {
+            this.Command?.Execute((MidiPercussionNote)set); // Command
+        }
+    }
+}
