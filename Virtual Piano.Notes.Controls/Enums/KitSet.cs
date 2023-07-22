8688eb7b3f1b209f2a33004e140f3a75034b043a
ysdy44 <ysdy44@outlook.com>
2023-07-23 03:55:17 +0800

    Add KitSet

---
+++ Virtual Piano.Notes.Controls/Enums/KitSet.cs
@@ -0,0 +1,19 @@
+﻿namespace Virtual_Piano.Notes.Controls
+{
+    public enum KitSet : byte
+    {
+        Crash = MidiPercussionNote.CrashCymbal1,
+        Ride = MidiPercussionNote.RideCymbal1,
+        Open = MidiPercussionNote.OpenHiHat,
+        Close = MidiPercussionNote.ClosedHiHat,
+        Pedal = MidiPercussionNote.PedalHiHat,
+
+        HiTom = MidiPercussionNote.HighTom,
+        LowTom = MidiPercussionNote.LowTom,
+        FloorTom = MidiPercussionNote.LowFloorTom,
+
+        Snare = MidiPercussionNote.AcousticSnare,
+        Stick = MidiPercussionNote.SideStick,
+        Kick = MidiPercussionNote.BassDrum1,
+    }
+}
