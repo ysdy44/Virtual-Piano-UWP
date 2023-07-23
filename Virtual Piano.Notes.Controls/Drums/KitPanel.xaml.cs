4f02f4b276192782e2e245b7bbbceb73e8d80bc5
ysdy44 <ysdy44@outlook.com>
2023-07-23 16:34:25 +0800

    KitPanel 2.0.0

--- Virtual Piano.Notes.Controls/Drums/KitPanel.xaml.cs
+++ Virtual Piano.Notes.Controls/Drums/KitPanel.xaml.cs
@@ -32,6 +32,42 @@ namespace Virtual_Piano.Notes.Controls
         public void OnClick(KitSet set)
         {
             this.Command?.Execute((MidiPercussionNote)set); // Command
+
+            switch (set)
+            {
+                case KitSet.Crash:
+                    this.CrashStoryboard.Stop(); // Storyboard
+                    this.CrashStoryboard.Begin(); // Storyboard
+                    break;
+                case KitSet.Ride:
+                    this.RideStoryboard.Stop(); // Storyboard
+                    this.RideStoryboard.Begin(); // Storyboard
+                    break;
+                case KitSet.Open:
+                    break;
+                case KitSet.Close:
+                    break;
+                case KitSet.Pedal:
+                    break;
+                case KitSet.HiTom:
+                    break;
+                case KitSet.LowTom:
+                    break;
+                case KitSet.FloorTom:
+                    break;
+                case KitSet.Snare:
+                    break;
+                case KitSet.Stick:
+                    break;
+                case KitSet.Kick:
+                    this.KickStoryboard1.Stop(); // Storyboard
+                    this.KickStoryboard2.Stop(); // Storyboard
+                    this.KickStoryboard1.Begin(); // Storyboard
+                    this.KickStoryboard2.Begin(); // Storyboard
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
