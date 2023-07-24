7d74571e9ff3cc3d06eca43d740d5e270085cfef
ysdy44 <ysdy44@outlook.com>
2023-07-25 02:35:23 +0800

    Add HarpPanel

---
+++ Virtual Piano.Midi.Controls/Instruments/Harps/HarpButton.cs
@@ -0,0 +1,16 @@
+﻿namespace Virtual_Piano.Midi.Controls
+{
+    public sealed class HarpButton : ClickButton
+    {
+        //@Command
+        public MidiNote CommandParameter { get; set; }
+
+        protected override void OnClick()
+        {
+            if (base.Parent is INotePanel item)
+            {
+                item.OnClick(this.CommandParameter);
+            }
+        }
+    }
+}
---
+++ Virtual Piano.Midi.Controls/Instruments/Harps/HarpPanel.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Midi.Controls
+{
+    public partial class HarpPanel : StackPanel, INotePanel
+    {
+        //@Command
+        public ICommand Command { get; set; }
+
+        //@Const
+        const Octave Number = Octave.Number3;
+        const int Min = (int)HarpPanel.Number;
+        const int Max = HarpPanel.Min + 7;
+
+        //@Construct
+        public HarpPanel()
+        {
+            this.InitializeComponent();
+
+            for (int y = HarpPanel.Min; y < HarpPanel.Max; y++)
+            {
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.C + y * 12),
+                    Style = base.Resources[$"{HarpType.Red}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.D + y * 12),
+                    Style = base.Resources[$"{HarpType.White}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.E + y * 12),
+                    Style = base.Resources[$"{HarpType.White}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.F + y * 12),
+                    Style = base.Resources[$"{HarpType.Blue}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.G + y * 12),
+                    Style = base.Resources[$"{HarpType.White}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.A + y * 12),
+                    Style = base.Resources[$"{HarpType.White}"] as Style,
+                });
+                base.Children.Add(new HarpButton
+                {
+                    CommandParameter = (MidiNote)(Tone.B + y * 12),
+                    Style = base.Resources[$"{HarpType.White}"] as Style,
+                });
+            }
+        }
+
+        public void OnClick(MidiNote note)
+        {
+            this.Command?.Execute(note); // Command
+        }
+    }
+}
