0df34ff70ce8f1c6c2d7bdb5bbd2a2ad1231e4e4
ysdy44 <ysdy44@outlook.com>
2023-07-27 00:46:20 +0800

    Favorites Add Emoji

--- Virtual Piano.Midi.Controls/Instruments/Instruments/InstrumentObservableCollection.cs
+++ Virtual Piano.Midi.Controls/Instruments/Instruments/InstrumentObservableCollection.cs
@@ -17,11 +17,33 @@ namespace Virtual_Piano.Midi.Controls
                 this.OnPropertyChanged(nameof(Instrument));
                 this.OnPropertyChanged(nameof(IsContains));
                 this.OnPropertyChanged(nameof(String));
+                this.OnPropertyChanged(nameof(Emoji));
             }
         }
 
         public bool IsContains => base.Contains(this.Instrument);
         public string String => this.GetString(this.Instrument);
+        public string Emoji
+        {
+            get
+            {
+                foreach (var item in MidiProgramFactory.Instance)
+                {
+                    foreach (var item2 in item.Value)
+                    {
+                        foreach (var item3 in item2.Value)
+                        {
+                            if (item3 == this.Instrument)
+                            {
+                                return MidiProgramFactory.Emoji[item2.Key];
+                            }
+                        }
+                    }
+                }
+
+                return string.Empty;
+            }
+        }
 
         public virtual string GetString(MidiProgram program)
         {
