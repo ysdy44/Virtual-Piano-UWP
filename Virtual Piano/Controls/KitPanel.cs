a8861414857a50f7556317ce453ef788d2a97f14
ysdy44 <ysdy44@outlook.com>
2023-07-29 10:46:52 +0800

    Add KitPanel

---
+++ Virtual Piano/Controls/KitPanel.cs
@@ -0,0 +1,12 @@
+﻿using Virtual_Piano.Midi;
+
+namespace Virtual_Piano.Controls
+{
+    public sealed class KitPanel : Midi.Controls.KitPanel
+    {
+        public override string GetString(MidiPercussionNote note)
+        {
+            return App.Resource.GetString($"{note}");
+        }
+    }
+}
