8d917064c067ebe160f148631edd9cf6e4ed2bb0
ysdy44 <ysdy44@outlook.com>
2023-07-29 15:25:20 +0800

    Remove BendWheel0 and BendWheel64

--- Virtual Piano.Midi.Controls/Controllers/Bends/BendModulationWheel.cs
+++ Virtual Piano.Midi.Controls/Controllers/Bends/BendModulationWheel.cs
@@ -3,17 +3,21 @@ using Windows.Devices.Midi;
 
 namespace Virtual_Piano.Midi.Controls
 {
-    public sealed class BendModulationWheel : BendWheel0
+    public sealed class BendModulationWheel : BendWheel
     {
         //@Command
         public ICommand Command { get; set; }
         public MidiControlController Controller { get; set; } = MidiControlController.Modulation;
 
+        public BendModulationWheel() : base(0) { }
+
+        public override void Start() { }
+        public override void Stop() { }
         public override void Execute(int value)
         {
             this.Command?.Execute(new MidiMessage
             {
-                Type = MidiMessageType.NoteOn,
+                Type = MidiMessageType.ControlChange,
                 Controller = this.Controller,
                 ControllerValue = (byte)value
             }); // Command
