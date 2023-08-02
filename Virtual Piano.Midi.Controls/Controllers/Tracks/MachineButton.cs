a54f8e48f581afe69ab4e9e6b793d4f341d2dbd7
ysdy44 <ysdy44@outlook.com>
2023-08-03 01:50:05 +0800

    Add MachineButton

---
+++ Virtual Piano.Midi.Controls/Controllers/Tracks/MachineButton.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Virtual_Piano.Midi.Controls
+{
+    public sealed class MachineButton : ToggleButton, IMachineButton
+    {
+        public double X { get => Canvas.GetLeft(this); set => Canvas.SetLeft(this, value); }
+        public double Y { get => Canvas.GetTop(this); set => Canvas.SetTop(this, value); }
+
+        public MachineButton()
+        {
+            this.Clear();
+            base.Unchecked += (s, e) => this.Clear();
+            base.Checked += (s, e) => this.Add();
+        }
+
+        public void Add() => base.AllowDrop = true;
+        public void Clear() => base.AllowDrop = false;
+    }
+}
---
+++ Virtual Piano.Midi.Controls/Interfaces/Buttons/IMachineButton.cs
@@ -0,0 +1,9 @@
+﻿namespace Virtual_Piano.Midi.Controls
+{
+    public interface IMachineButton : IClickButton
+    {
+        bool AllowDrop { get; set; }
+        bool IsEnabled { get; set; }
+        bool? IsChecked { get; set; }
+    }
+}
