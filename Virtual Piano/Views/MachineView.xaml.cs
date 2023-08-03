ba554199b9573522dc54d40a4eefa71f26654beb
ysdy44 <ysdy44@outlook.com>
2023-08-04 01:19:50 +0800

    Add MachineView

---
+++ Virtual Piano/Views/MachineView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Virtual_Piano.Views
+{
+    public sealed partial class MachineView : Page
+    {
+        ICommand Command;
+
+        public MachineView()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.Command = null;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Command = e.Parameter as ICommand;
+        }
+    }
+}
