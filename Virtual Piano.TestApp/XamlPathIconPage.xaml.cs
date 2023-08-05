6447c5126b484f9a8bc38d7710185ed3a578f49d
ysdy44 <ysdy44@outlook.com>
2023-08-06 02:18:12 +0800

    Add XamlPathIconPage

---
+++ Virtual Piano.TestApp/XamlPathIconPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Virtual_Piano.Elements;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class XamlPathIconPage : Page
+    {
+        public XamlPathIconPage()
+        {
+            this.InitializeComponent();
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Piano"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Chord"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Machine"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Drum"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Guitar"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Bass"].ToGeometry() });
+            this.StackPanel.Children.Add(new PathIcon { Data = base.Resources["Harp"].ToGeometry() });
+        }
+    }
+}
