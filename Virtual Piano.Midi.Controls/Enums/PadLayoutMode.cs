465e572cffb16c2f24f72037c1d2bf4c65dddf54
ysdy44 <ysdy44@outlook.com>
2023-08-10 00:00:22 +0800

    Add PadLayoutMode

---
+++ Virtual Piano.Midi.Controls/Enums/PadLayoutMode.cs
@@ -0,0 +1,12 @@
+﻿namespace Virtual_Piano.Midi.Controls
+{
+    public enum PadLayoutMode // n =12 * width / height
+    {
+        L1x12, // n = 12 * 1 / 12 = 1 (n<2)
+        L2x6, // n = 12 * 2 / 6 = 4 (n<7)
+        L3x4, // n = 12 * 3 / 4 = 9 (n<12)
+        L4x3, // n = 12 * 4 / 3 = 16 (n<26)
+        L6x2, // n = 12 * 6 / 2 = 36 (n<54)
+        L12x1, // n = 12 * 12 / 1 = 144
+    }
+}
