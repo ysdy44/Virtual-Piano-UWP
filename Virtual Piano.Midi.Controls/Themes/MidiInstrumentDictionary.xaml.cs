3f9f09a3076d6ef192b4b897e5a3eedfd1962313
ysdy44 <ysdy44@outlook.com>
2023-08-10 01:56:06 +0800

    Add InstrumentDictionary

---
+++ Virtual Piano.Midi.Controls/Themes/MidiInstrumentDictionary.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Virtual_Piano.Midi.Controls
+{
+    public sealed partial class InstrumentDictionary : ResourceDictionary
+    {
+        public Geometry this[MidiInstrument instrument] => this.FindGeometry($"{instrument}");
+        public InstrumentDictionary()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
