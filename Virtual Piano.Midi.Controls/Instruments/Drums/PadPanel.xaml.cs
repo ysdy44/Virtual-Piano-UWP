22a1f5eb158fd5e6d77110817e5fb6d6ed938226
ysdy44 <ysdy44@outlook.com>
2023-08-10 00:04:36 +0800

    IKitPanel 3.0.0

--- Virtual Piano.Midi.Controls/Instruments/Drums/PadPanel.xaml.cs
+++ Virtual Piano.Midi.Controls/Instruments/Drums/PadPanel.xaml.cs
@@ -11,8 +11,40 @@ namespace Virtual_Piano.Midi.Controls
         //@Command
         public ICommand Command { get; set; }
 
+        private readonly IList<KitSet> Pads1x12 = new List<KitSet>
+        {
+            KitSet.Kick,
+            KitSet.Snare,
+            KitSet.Clap,
+            KitSet.Close,
+            KitSet.LowTom,
+            KitSet.Open,
+            KitSet.MidTom,
+            KitSet.Crash,
+            KitSet.FloorTom,
+            KitSet.Ride,
+            KitSet.Ring,
+            KitSet.Sandham,
+        };
 
-        private readonly IList<KitSet> ItemsSource = new List<KitSet>
+        private readonly IList<KitSet> Pads2x6 = new List<KitSet>
+        {
+            KitSet.Open,
+            KitSet.Crash,
+            KitSet.LowTom,
+            KitSet.MidTom,
+            KitSet.FloorTom,
+            KitSet.Ride,
+
+            KitSet.Close,
+            KitSet.Sandham,
+            KitSet.Clap,
+            KitSet.Ring,
+            KitSet.Snare,
+            KitSet.Kick,
+        };
+
+        private readonly IList<KitSet> Pads3x4 = new List<KitSet>
         {
             KitSet.Open,
             KitSet.Ride,
@@ -39,26 +71,73 @@ namespace Virtual_Piano.Midi.Controls
                 if (e.NewSize == Size.Empty) return;
                 if (e.NewSize == e.PreviousSize) return;
 
-                DrumLayout size = new DrumLayout((int)e.NewSize.Width, (int)e.NewSize.Height);
-                for (int y = 0; y < size.Row; y++)
+                PadLayout size = new PadLayout((int)e.NewSize.Width, (int)e.NewSize.Height);
+                switch (size.Mode)
                 {
-                    for (int x = 0; x < size.Column; x++)
-                    {
-                        int i = size.Column * y + x;
-                        if (base.Children[i] is IKitButton item)
+                    case PadLayoutMode.L1x12:
+                    case PadLayoutMode.L12x1:
+                        for (int y = 0; y < size.Row; y++)
                         {
-                            item.X = x * size.Width;
-                            item.Y = y * size.Height;
-                            item.Width = size.Width;
-                            item.Height = size.Height;
+                            for (int x = 0; x < size.Column; x++)
+                            {
+                                int i = size.Column * y + x;
+                                int index = i;
+                                if (base.Children[index] is IDrumButton item)
+                                {
+                                    item.X = x * size.Width;
+                                    item.Y = y * size.Height;
+                                    item.Width = size.Width;
+                                    item.Height = size.Height;
+                                }
+                            }
                         }
-                    }
+                        break;
+                    case PadLayoutMode.L2x6:
+                    case PadLayoutMode.L6x2:
+                        for (int y = 0; y < size.Row; y++)
+                        {
+                            for (int x = 0; x < size.Column; x++)
+                            {
+                                int i = size.Column * y + x;
+                                KitSet note = this.Pads2x6[i];
+                                int index = this.Pads1x12.IndexOf(note);
+                                if (base.Children[index] is IDrumButton item)
+                                {
+                                    item.X = x * size.Width;
+                                    item.Y = y * size.Height;
+                                    item.Width = size.Width;
+                                    item.Height = size.Height;
+                                }
+                            }
+                        }
+                        break;
+                    case PadLayoutMode.L3x4:
+                    case PadLayoutMode.L4x3:
+                        for (int y = 0; y < size.Row; y++)
+                        {
+                            for (int x = 0; x < size.Column; x++)
+                            {
+                                int i = size.Column * y + x;
+                                KitSet note = this.Pads3x4[i];
+                                int index = this.Pads1x12.IndexOf(note);
+                                if (base.Children[index] is IDrumButton item)
+                                {
+                                    item.X = x * size.Width;
+                                    item.Y = y * size.Height;
+                                    item.Width = size.Width;
+                                    item.Height = size.Height;
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
                 }
             };
 
-            foreach (KitSet set in this.ItemsSource)
+            foreach (KitSet set in this.Pads1x12)
             {
-                MidiPercussionNote item = (MidiPercussionNote)set;
+                var item = (MidiPercussionNote)set;
                 base.Children.Add(new KitButton
                 {
                     Content = $"{this.GetString(item)}",
--- Virtual Piano.Midi.Controls/Structs/PadLayout.cs
+++ Virtual Piano.Midi.Controls/Structs/PadLayout.cs
@@ -1,29 +1,47 @@
 ﻿namespace Virtual_Piano.Midi.Controls
 {
-    public readonly struct DrumLayout
+    public readonly struct PadLayout
     {
+        public readonly PadLayoutMode Mode;
+
         public readonly int Width;
         public readonly int Height;
 
         public readonly int Column;
         public readonly int Row;
 
-        public DrumLayout(int width, int height)
+        public PadLayout(int width, int height)
         {
             int n = height > 0 ? 12 * width / height : 1;
 
-            // 1x12: n = 12 * 1 / 12 = 1 (n<2)
-            // 2x6: n = 12 * 2 / 6 = 4 (n<7)
-            // 3x4: n = 12 * 3 / 4 = 9 (n<12)
-            // 4x3: n = 12 * 4 / 3 = 16 (n<26)
-            // 6x2: n = 12 * 6 / 2 = 36 (n<54)
-            // 12x1: n = 12 * 12 / 1 = 144
-            if (n < 2) { this.Column = 1; this.Row = 12; }
-            else if (n < 7) { this.Column = 2; this.Row = 6; }
-            else if (n < 12) { this.Column = 3; this.Row = 4; }
-            else if (n < 26) { this.Column = 4; this.Row = 3; }
-            else if (n < 54) { this.Column = 6; this.Row = 2; }
-            else { this.Column = 12; this.Row = 1; }
+            if (n < 2) this.Mode = PadLayoutMode.L1x12;
+            else if (n < 7) this.Mode = PadLayoutMode.L2x6;
+            else if (n < 12) this.Mode = PadLayoutMode.L3x4;
+            else if (n < 26) this.Mode = PadLayoutMode.L4x3;
+            else if (n < 54) this.Mode = PadLayoutMode.L6x2;
+            else this.Mode = PadLayoutMode.L12x1;
+
+            switch (this.Mode)
+            {
+                case PadLayoutMode.L1x12: this.Column = 1; break;
+                case PadLayoutMode.L2x6: this.Column = 2; break;
+                case PadLayoutMode.L3x4: this.Column = 3; break;
+                case PadLayoutMode.L4x3: this.Column = 4; break;
+                case PadLayoutMode.L6x2: this.Column = 6; break;
+                case PadLayoutMode.L12x1: this.Column = 12; break;
+                default: this.Column = 12; break;
+            }
+
+            switch (this.Mode)
+            {
+                case PadLayoutMode.L1x12: this.Row = 12; break;
+                case PadLayoutMode.L2x6: this.Row = 6; break;
+                case PadLayoutMode.L3x4: this.Row = 4; break;
+                case PadLayoutMode.L4x3: this.Row = 3; break;
+                case PadLayoutMode.L6x2: this.Row = 2; break;
+                case PadLayoutMode.L12x1: this.Row = 1; break;
+                default: this.Row = 1; break;
+            }
 
             this.Width = width / this.Column;
             this.Height = height / this.Row;
