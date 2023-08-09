b91047d3911a511eb90e8b14e13fa3c8902d67c1
ysdy44 <ysdy44@outlook.com>
2023-08-10 00:44:35 +0800

    Add CompositionPage

---
+++ Virtual Piano.TestApp/CompositionPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Virtual_Piano.Midi.Controls;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class CompositionPage : Page
+    {
+        readonly Windows.UI.Composition.CompositionPropertySet ScrollProperties;
+        ~CompositionPage() => this.ScrollProperties.Dispose();
+        public CompositionPage()
+        {
+            this.InitializeComponent();
+            this.ScrollProperties = this.ScrollViewer.GetScroller();
+            var x = this.ScrollProperties.SnapScrollerX();
+            var y = this.ScrollProperties.SnapScrollerY();
+            this.Left.GetVisual().AnimationX(x);
+            this.Top.GetVisual().AnimationY(y);
+            this.LeftTop.GetVisual().AnimationXY(x, y);
+            this.LeftBottom.GetVisual().AnimationX(x);
+
+            base.SizeChanged += (s, e) =>
+            {
+                if (e.NewSize == Size.Empty) return;
+                if (e.NewSize == e.PreviousSize) return;
+                if ((int)e.NewSize.Height == (int)e.PreviousSize.Height) return;
+
+                var y2 = this.ScrollProperties.SnapScrollerY((int)e.NewSize.Height - 75);
+                this.Bottom.GetVisual().AnimationY(y2);
+                this.LeftBottom.GetVisual().AnimationY(y2);
+            };
+        }
+    }
+}
