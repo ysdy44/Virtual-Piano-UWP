69f984e098dfb217152c6367bc47750877b33783
ysdy44 <ysdy44@outlook.com>
2023-08-10 01:57:24 +0800

    Add MidiPercussionNoteCategoryDictionary

---
+++ Virtual Piano.Midi.Controls/Themes/MidiPercussionNoteCategoryDictionary.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Virtual_Piano.Midi.Controls
+{
+    public sealed partial class MidiPercussionNoteCategoryDictionary : ResourceDictionary
+    {
+        public Brush this[MidiPercussionNoteCategory category] => this.FindBrush($"{category}");
+        public MidiPercussionNoteCategoryDictionary()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
