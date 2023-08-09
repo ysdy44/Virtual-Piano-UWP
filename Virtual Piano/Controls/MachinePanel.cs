3655e68a2796330c4a50809b4982731c419db8d5
ysdy44 <ysdy44@outlook.com>
2023-08-10 02:06:30 +0800

    Add MachinePanel

---
+++ Virtual Piano/Controls/MachinePanel.cs
@@ -0,0 +1,12 @@
+﻿using Virtual_Piano.Midi;
+
+namespace Virtual_Piano.Controls
+{
+    public sealed class MachinePanel : Midi.Controls.MachinePanel
+    {
+        public override string GetString(MidiPercussionNote note)
+        {
+            return App.Resource.GetString($"{note}");
+        }
+    }
+}
