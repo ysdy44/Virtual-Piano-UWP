3aef1f01fcd2e899fff45d5cf0dcee4d4ea0633e
ysdy44 <ysdy44@outlook.com>
2023-08-12 13:51:35 +0800

    File move: Controls -> Core

--- Virtual Piano.Midi.Core/Properties/AssemblyInfo.cs
+++ Virtual Piano.Midi.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Virtual Piano.Midi.Controls")]
+[assembly: AssemblyTitle("Virtual Piano.Midi.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Virtual Piano.Midi.Controls")]
+[assembly: AssemblyProduct("Virtual Piano.Midi.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
