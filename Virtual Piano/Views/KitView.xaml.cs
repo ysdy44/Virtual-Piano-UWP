795ec87851d98461a2d27086c38acdc83b79adf3
ysdy44 <ysdy44@outlook.com>
2023-08-13 14:37:19 +0800

    Add KitView and PadView

---
+++ Virtual Piano/Views/KitView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Virtual_Piano.Views
+{
+    public sealed partial class KitView : Page
+    {
+        ICommand Command;
+
+        public KitView()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            this.Command = null;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.Command = e.Parameter as ICommand;
+        }
+    }
+}
