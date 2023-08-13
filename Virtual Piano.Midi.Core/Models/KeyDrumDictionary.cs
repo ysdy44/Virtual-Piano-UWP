eb31dfd212a5ab43c577409f9d59c6a41463f140
ysdy44 <ysdy44@outlook.com>
2023-08-13 17:08:52 +0800

    Add KeyDrumDictionary

---
+++ Virtual Piano.Midi.Core/Models/KeyDrumDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Windows.System;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public class KeyDrumDictionary : Dictionary<VirtualKey, MidiPercussionNote>
+    {
+        public KeyDrumDictionary()
+        {
+            this[VirtualKey.F1] = (MidiPercussionNote)KitSet.Kick;
+            this[VirtualKey.F2] = (MidiPercussionNote)KitSet.Snare;
+            this[VirtualKey.F3] = (MidiPercussionNote)KitSet.Clap;
+            this[VirtualKey.F4] = (MidiPercussionNote)KitSet.Close;
+            this[VirtualKey.F5] = (MidiPercussionNote)KitSet.LowTom;
+            this[VirtualKey.F6] = (MidiPercussionNote)KitSet.Open;
+            this[VirtualKey.F7] = (MidiPercussionNote)KitSet.MidTom;
+            this[VirtualKey.F8] = (MidiPercussionNote)KitSet.Crash;
+            this[VirtualKey.F9] = (MidiPercussionNote)KitSet.FloorTom;
+            this[VirtualKey.F10] = (MidiPercussionNote)KitSet.Ride;
+            this[VirtualKey.F11] = (MidiPercussionNote)KitSet.Ring;
+            this[VirtualKey.F12] = (MidiPercussionNote)KitSet.Sandham;
+        }
+    }
+}
