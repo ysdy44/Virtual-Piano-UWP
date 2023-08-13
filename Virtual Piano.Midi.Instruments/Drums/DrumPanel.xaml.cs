724fe68a094c4726ce6de47440ceb0d78e8def66
ysdy44 <ysdy44@outlook.com>
2023-08-13 16:47:27 +0800

    DrumPanel 4.0.0: Panel is Canvas

--- Virtual Piano.Midi.Instruments/Drums/DrumPanel.xaml.cs
+++ Virtual Piano.Midi.Instruments/Drums/DrumPanel.xaml.cs
@@ -1,17 +1,39 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Controls;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using Virtual_Piano.Midi.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Virtual_Piano.Midi.Instruments
 {
-    public partial class DrumPanel : WrapPanel, IDrumPanel
+    public partial class DrumPanel : Canvas, IDrumPanel
     {
         //@Command
         public ICommand Command { get; set; }
 
         readonly MidiPercussionNoteCategoryDictionary MidiPercussionNoteCategoryDictionary = new MidiPercussionNoteCategoryDictionary();
         readonly MidiPercussionNoteDictionary MidiPercussionNoteDictionary = new MidiPercussionNoteDictionary();
+
+        private int widthCount = 5;
+        public int WidthCount
+        {
+            get => this.widthCount;
+            set
+            {
+                if (this.widthCount == value) return;
+                this.widthCount = value;
+
+                for (int i = 0; i < base.Children.Count; i++)
+                {
+                    UIElement item = base.Children[i];
+                    Canvas.SetLeft(item, (i % value) * 100);
+                    Canvas.SetTop(item, (i / value) * 120);
+                }
+
+                int count = base.Children.Count;
+                base.Width = value * 100;
+                base.Height = (count / value) * 120;
+            }
+        }
 
         //@Construct
         public DrumPanel()
@@ -23,6 +45,8 @@ namespace Virtual_Piano.Midi.Instruments
                 {
                     base.Children.Add(new DrumButton
                     {
+                        Width = 100,
+                        Height = 120,
                         Content = $"{this.GetString(item3)}",
                         CommandParameter = item3,
                         TabIndex = (byte)item3,
@@ -40,6 +64,11 @@ namespace Virtual_Piano.Midi.Instruments
         public virtual string GetString(MidiPercussionNote note)
         {
             return note.ToString();
+        }
+
+        public void UpdateWidthCount(double width)
+        {
+            this.WidthCount = (int)(width / 100);
         }
     }
 }
