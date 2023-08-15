e4ddf6c4a210a62b99f482e0f885d73565fb503d
ysdy44 <ysdy44@outlook.com>
2023-08-16 01:29:57 +0800

    MidiMessage 2.0.0

--- Virtual Piano.Midi/Messages/MidiMessage.cs
+++ Virtual Piano.Midi/Messages/MidiMessage.cs
@@ -2,6 +2,7 @@
 
 namespace Virtual_Piano.Midi
 {
+    /// <summary> <see cref="IMidiMessage"/> </summary>
     public struct MidiMessage
     {
         public MidiMessageType Type;
@@ -9,17 +10,31 @@ namespace Virtual_Piano.Midi
         public int AbsoluteTime;
 
         public int Duration;
+        /// <summary> <see cref="MidiNoteOnMessage.Note"/> </summary>
         public MidiNote Note;
+        /// <summary> <see cref="MidiNoteOnMessage.Velocity"/> </summary>
         public byte Velocity;
+        /// <summary> <see cref="MidiNoteOnMessage"/> </summary>
 
+        /// <summary> <see cref="MidiControlChangeMessage.Controller"/> </summary>
         public MidiControlController Controller;
+        /// <summary> <see cref="MidiControlChangeMessage.ControlValue"/> </summary>
         public byte ControllerValue;
+        /// <summary> <see cref="MidiControlChangeMessage"/> </summary>
 
+        /// <summary> <see cref="MidiProgramChangeMessage.Program"/> </summary>
         public MidiProgram Program;
+        /// <summary> <see cref="MidiProgramChangeMessage"/> </summary>
 
+        /// <summary> <see cref="MidiChannelPressureMessage.Pressure"/> </summary>
         public byte Pressure;
+        /// <summary> <see cref="MidiChannelPressureMessage"/> </summary>
 
+        /// <summary> <see cref="MidiPitchBendChangeMessage.Bend"/> </summary>
         public ushort Bend;
+        /// <summary> <see cref="MidiPitchBendChangeMessage"/> </summary>
+
+        /// <summary> <see cref="MidiSystemExclusiveMessage"/> </summary>
 
         public override string ToString()
         {
@@ -27,9 +42,8 @@ namespace Virtual_Piano.Midi
             {
                 case MidiMessageType.NoteOff:
                 case MidiMessageType.NoteOn:
-                    return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Duration=\"{this.Duration}\" Note=\"{this.Note}\" Velocity=\"{this.Velocity}\"/>";
                 case MidiMessageType.PolyphonicKeyPressure:
-                    return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Pressure=\"{this.Pressure}\"/>";
+                    return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Duration=\"{this.Duration}\" Note=\"{this.Note}\" Velocity=\"{this.Velocity}\"/>";
                 case MidiMessageType.ControlChange:
                     return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Controller=\"{this.Controller}\" ControllerValue=\"{this.ControllerValue}\"/>";
                 case MidiMessageType.ProgramChange:
@@ -38,6 +52,7 @@ namespace Virtual_Piano.Midi
                     return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Pressure=\"{this.Pressure}\"/>";
                 case MidiMessageType.PitchBendChange:
                     return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\" Bend=\"{this.Bend}\"/>";
+                case MidiMessageType.SystemExclusive:
                 case MidiMessageType.SystemReset:
                     return $"<{this.Type} Channel=\"{this.Channel}\" AbsoluteTime=\"{this.AbsoluteTime}\"/>";
                 default:
