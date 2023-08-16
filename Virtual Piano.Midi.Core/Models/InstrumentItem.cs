456db41b1a15eccfc332f8c23a4a5616b94f8909
ysdy44 <ysdy44@outlook.com>
2023-08-16 23:37:55 +0800

    Add InstrumentItemGrouping and InstrumentItem

---
+++ Virtual Piano.Midi.Core/Models/InstrumentItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Virtual_Piano.Midi.Core
+{
+    public sealed class InstrumentItem
+    {
+        public MidiProgram Key { get; set; }
+        public string Text { get; set; }
+        public override string ToString() => $"{(int)this.Key}. {this.Text}";
+    }
+}
---
+++ Virtual Piano.Midi.Core/Models/InstrumentItemGrouping.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public sealed class InstrumentItemGrouping : List<InstrumentItem>, IList<InstrumentItem>, IGrouping<MidiProgramGroup, InstrumentItem>
+    {
+        public MidiProgramGroup Key { get; set; }
+        public string Text { get; set; }
+
+        public InstrumentItemGrouping(IEnumerable<InstrumentItem> collection) : base(collection) { }
+
+        public override string ToString() => this.Text;
+    }
+}
