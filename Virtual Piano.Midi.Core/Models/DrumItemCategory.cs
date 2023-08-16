c4c595a5dff24dd06cceca32dd0f77c9b85b9f0b
ysdy44 <ysdy44@outlook.com>
2023-08-16 13:27:48 +0800

    Delete DrumItemCategory

--- Virtual Piano.Midi.Core/Models/DrumItemCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Virtual_Piano.Midi.Core
-{
-    public class DrumItemCategory
-    {
-        public MidiPercussionNoteCategory Category;
-        public string Text;
-        public override string ToString() => this.Text;
-    }
-}
