bb6b19d4e0af9db58f5bf2e527fa880d47a95a49
ysdy44 <ysdy44@outlook.com>
2023-08-16 13:19:44 +0800

    DrumMachinePage 4.0.0: use MachinePanel

--- Virtual Piano.TestApp/DrumMachinePage.xaml.cs
+++ Virtual Piano.TestApp/DrumMachinePage.xaml.cs
@@ -1,113 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Virtual_Piano.Midi;
 using Windows.Devices.Midi;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace Virtual_Piano.TestApp
 {
-    public sealed class DrumMachine : List<ListViewItem>
-    {
-        public int Index { get; private set; } = 0;
-        public int Length { get; private set; } = 32;
-
-        public ListViewItem this[int n, int i]
-        {
-            get => this[n + i * 4];
-            set => this[n + i * 4] = value;
-        }
-
-        public DrumMachine()
-        {
-            for (int i = 0; i < 32; i++)
-            {
-                for (int n = 0; n < 4; n++)
-                {
-                    base.Add(new ListViewItem
-                    {
-                        Content = i
-                    });
-                }
-            }
-        }
-
-        public void Initialize(byte[] bytes)
-        {
-            this.Index = 0;
-            this.Length = bytes.Length / 4;
-
-            for (int i = 0; i < this.Length; i++)
-            {
-                for (int n = 0; n < 4; n++)
-                {
-                    var index = n + i * 4;
-                    if (this[index] is ListViewItem item)
-                    {
-                        item.IsEnabled = true;
-                        item.IsSelected = bytes[index] is 1;
-                    }
-                }
-            }
-
-            for (int i = this.Length; i < 32; i++)
-            {
-                for (int n = 0; n < 4; n++)
-                {
-                    if (this[n, i] is ListViewItem item)
-                    {
-                        item.IsEnabled = false;
-                        item.IsSelected = false;
-                    }
-                }
-            }
-        }
-
-        public void Deselect()
-        {
-            foreach (ListViewItem item in this)
-            {
-                item.IsSelected = false;
-            }
-        }
-
-        public void Next()
-        {
-            this.Index++;
-            if (this.Index < this.Length) return;
-            this.Index = 0;
-        }
-    }
-
     public sealed partial class DrumMachinePage : Page, ICommand
     {
         readonly KitSet[] Drum = new KitSet[] { KitSet.Open, KitSet.Close, KitSet.Clap, KitSet.Kick, };
-        readonly ListViewItem[] HeadItemsSource;
-        readonly DrumMachine ItemsSource = new DrumMachine();
-        readonly DispatcherTimer Timer = new DispatcherTimer();
 
         MidiSynthesizer Synthesizer;
         ~DrumMachinePage() => this.Synthesizer?.Dispose();
         public DrumMachinePage()
         {
             this.InitializeComponent();
-            this.HeadItemsSource = Drum.Select(c => new ListViewItem
-            {
-                Content = c
-            }).ToArray();
-
-            Tempo tempo = new Tempo(100);
+            Tempo tempo = this.ItemsSource.Tempo;
             this.Run.Text = $"{tempo.Bpm}";
-            this.Timer.Interval = (tempo.Delay);
             this.Slider.Value = tempo.Bpm;
             this.Slider.ValueChanged += (s, e) =>
             {
                 Tempo t = new Tempo((int)e.NewValue);
                 this.Run.Text = $"{t.Bpm}";
-                this.Timer.Interval = (t.Delay);
+                this.ItemsSource.Tempo = t;
             };
 
             base.Loaded += (s, e) => this.Initialize(0);
@@ -126,30 +43,6 @@ namespace Virtual_Piano.TestApp
             }
 
             this.ClearButton.Click += (s, e) => this.ItemsSource.Deselect();
-            this.StopButton.Click += (s, e) => this.Timer.Stop();
-            this.PlayButton.Click += (s, e) => this.Timer.Start();
-
-            this.Timer.Tick += (s, e) =>
-            {
-                int i = this.ItemsSource.Index;
-
-                for (int n = 0; n < 4; n++)
-                {
-                    MidiPercussionNote note = (MidiPercussionNote)this.Drum[n];
-                    this.Synthesizer.NoteOff(note);
-
-                    if (this.ItemsSource[n, i] is ListViewItem item)
-                    {
-                        if (item.IsSelected)
-                        {
-                            this.Synthesizer.NoteOn(note);
-                        }
-                    }
-                }
-
-                this.ItemsSource.Next();
-                this.ProgressBar.Value = this.ItemsSource.Index;
-            };
         }
 
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e) => this.Synthesizer?.Dispose();
@@ -161,11 +54,17 @@ namespace Virtual_Piano.TestApp
         //@Command
         public ICommand Command => this;
         public bool CanExecute(object parameter) => true;
-        public void Execute(object parameter)
+        public async void Execute(object parameter)
         {
             if (parameter is int item)
             {
                 this.Initialize(item);
+            }
+            if (parameter is MidiPercussionNote item2)
+            {
+                this.Synthesizer.NoteOn(item2);
+                await Task.Delay(2000);
+                this.Synthesizer.NoteOff(item2);
             }
         }
 
