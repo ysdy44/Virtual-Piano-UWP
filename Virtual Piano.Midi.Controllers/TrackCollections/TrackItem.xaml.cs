e5f57b9717658fc9afcfd75d0aafb953c5869ed2
ysdy44 <ysdy44@outlook.com>
2023-08-16 12:59:16 +0800

    Add TrackItem

---
+++ Virtual Piano.Midi.Controllers/TrackCollections/TrackItem.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Virtual_Piano.Midi.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    public sealed partial class TrackItem : UserControl
+    {
+        const int Count2 = NoteExtensions.NoteCount / 2;
+        const int Count4 = Count2 / 2;
+
+        public string Text { get => this.TextBlock.Text; set => this.TextBlock.Text = value; }
+
+        public TrackItem(MidiTrack info)
+        {
+            this.InitializeComponent();
+
+            foreach (var item in info.Notes)
+            {
+                var x = item.AbsoluteTime / TrackNoteLayout.Scaling;
+                var y = item.Note.ToInedx();
+                var w = System.Math.Max(4, item.Duration / TrackNoteLayout.Scaling);
+
+                this.Canvas.Children.Add(new Line
+                {
+                    X1 = x,
+                    Y1 = y - Count4,
+                    X2 = x + w,
+                    Y2 = y - Count4
+                });
+            }
+        }
+    }
+}
