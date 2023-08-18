792ff5f191f34067bc731a244dd833af9a50d4ce
ysdy44 <ysdy44@outlook.com>
2023-08-19 00:40:54 +0800

    Add UIType

---
+++ Virtual Piano/Enums/UIType.cs
@@ -0,0 +1,85 @@
+﻿namespace Virtual_Piano
+{
+    public enum UIType
+    {
+        Name,
+
+        Version,
+        Github,
+        Feedback,
+
+
+        VersionTitle,
+        GithubTitle,
+        FeedbackTitle,
+
+        GithubLink,
+        FeedbackLink,
+
+
+        Cancel,
+        OK,
+
+        About,
+        Setting,
+        Split,
+        More,
+        Tutorial,
+
+        Metronome,
+
+        Mute,
+
+        FullScreen,
+        UnFullScreen,
+
+        Favorite,
+        UnFavorite,
+
+        Theme,
+        ThemeLight,
+        ThemeDark,
+        ThemeUseSystem,
+
+        Language,
+        LanguageTip,
+        LanguageUseSystemSetting,
+
+
+        DockLeft,
+        DockRight,
+        DockBottom,
+
+        Bends,
+        BendPitch,
+        BendModulation,
+        BendPressure,
+
+        Pedals,
+        PedalSustain,
+        PedalSoftPedal,
+        PedalSostenuto,
+        PedalExpression,
+
+
+        PianoView,
+        ChordView,
+        GuitarView,
+        BassView,
+        HarpView,
+        KitView,
+        PadView,
+        DrumView,
+        MachineView,
+
+        TryShowPianoView,
+        TryShowChordView,
+        TryShowGuitarView,
+        TryShowBassView,
+        TryShowHarpView,
+        TryShowKitView,
+        TryShowPadView,
+        TryShowDrumView,
+        TryShowMachineView,
+    }
+}
---
+++ Virtual Piano/Strings/UIExtension.cs
@@ -0,0 +1,11 @@
+﻿using Windows.UI.Xaml.Markup;
+
+namespace Virtual_Piano.Strings
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public class UIExtension : MarkupExtension
+    {
+        public UIType Type { get; set; }
+        protected override object ProvideValue() => App.Resource.GetString(this.Type.ToString());
+    }
+}
