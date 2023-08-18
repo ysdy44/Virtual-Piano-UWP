1c7151db45df2c2507778cb3b14abefbc3426dd3
ysdy44 <ysdy44@outlook.com>
2023-08-18 13:32:47 +0800

    Add AnimationRouletterDictionary

---
+++ Virtual Piano.Midi.Core/Themes/AnimationRouletterDictionary.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Virtual_Piano.Midi.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public sealed partial class AnimationRouletterDictionary : ResourceDictionary
+    {
+        public Geometry this[int index] => this.FindGeometry($"Geometry{index}");
+        public AnimationRouletterDictionary()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
