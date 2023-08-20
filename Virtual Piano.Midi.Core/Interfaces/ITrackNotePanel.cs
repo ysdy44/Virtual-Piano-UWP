2a5d150ba54773aafdb487163d211fc7df213af3
ysdy44 <ysdy44@outlook.com>
2023-08-20 22:42:09 +0800

    Add ITrackNotePanel

---
+++ Virtual Piano.Midi.Core/Interfaces/ITrackNotePanel.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public interface ITrackNotePanel
+    {
+        //@Delegate
+        event DragStartedEventHandler DragStarted;
+        event DragDeltaEventHandler DragDelta;
+        event DragCompletedEventHandler DragCompleted;
+        event ItemClickEventHandler FootItemClick;
+        event RoutedEventHandler BackClick;
+
+        // Container
+        int ViewportWidth { get; }
+        int ViewportHeight { get; }
+
+        // Content
+        int HorizontalOffset { get; }
+        int VerticalOffset { get; }
+
+        int Index { get; set; }
+
+        // UI
+        object ItemsSource { get; set; }
+        UIElement Pane { get; set; }
+        PointCollection ControllerPoints { get; set; }
+        object ControllerItemsSource { get; set; }
+        object FootItemsSource { get; set; }
+        object HeadItemsSource { get; set; }
+
+        // Timeline
+        int Time { get; }
+
+        void ChangeDuration(int time);
+        void ChangePosition(int timeline);
+
+        int UpdateTimeline(int timeline);
+    }
+}
