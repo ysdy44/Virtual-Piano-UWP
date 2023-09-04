e7800b845e89da6a1104e21827a5d069a08eaa0c
ysdy44 <ysdy44@outlook.com>
2023-09-05 00:07:30 +0800

    Add AudioObservableCollection

---
+++ Virtual Piano.Midi.Core/Models/AudioObservableCollection.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+using System;
+using Windows.Media.Audio;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public sealed class AudioObservableCollection : ObservableCollection<ContentControl>, IAudioSynthesizer
+    {
+        public bool this[MidiNote note]
+        {
+            get
+            {
+                int index = (int)note;
+                foreach (ContentControl item in this)
+                {
+                    if (item.TabIndex == index)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public string Select(MidiNote note)
+        {
+            int index = (int)note;
+            foreach (ContentControl item in this)
+            {
+                if (item.TabIndex == index)
+                {
+                    return item.Content.ToString();
+                }
+            }
+            return string.Empty;
+        }
+
+        public ContentControl Select(int index)
+        {
+            foreach (ContentControl item in this)
+            {
+                if (item.TabIndex == index)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public void NoteOn(MidiNote note)
+        {
+            int index = (int)note;
+            foreach (ContentControl item in this)
+            {
+                if (item.TabIndex == index)
+                {
+                    if (item.Tag is AudioFileInputNode node)
+                    {
+                        node.Seek(TimeSpan.Zero);
+                        node.Start();
+                    }
+                }
+            }
+        }
+        public void NoteOff(MidiNote note)
+        {
+            int index = (int)note;
+            foreach (ContentControl item in this)
+            {
+                if (item.TabIndex == index)
+                {
+                    if (item.Tag is AudioFileInputNode node)
+                    {
+                        node.Stop();
+                    }
+                }
+            }
+        }
+
+        public void Reset() => this.Dispose();
+
+        public void Dispose()
+        {
+            foreach (ContentControl item in this)
+            {
+                item.Tag = null;
+            }
+            base.Clear();
+        }
+    }
+}
