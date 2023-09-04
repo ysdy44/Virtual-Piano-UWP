03a32c43eca745ac48c0d70849aebcf95dbaa13c
ysdy44 <ysdy44@outlook.com>
2023-09-05 00:07:10 +0800

    Add IAudioOutPort and IAudioSynthesizer

---
+++ Virtual Piano.Midi/Interfaces/IAudioOutPort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Audio;
+using Windows.Storage;
+
+namespace Virtual_Piano.Midi
+{
+    public interface IAudioOutPort : IDisposable
+    {
+        bool Remove(AudioFileInputNode node);
+        Task<AudioFileInputNode> AddAsync(IStorageFile file);
+    }
+}
---
+++ Virtual Piano.Midi/Interfaces/IAudioSynthesizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Virtual_Piano.Midi
+{
+    public interface IAudioSynthesizer : IDisposable
+    {
+        void NoteOff(MidiNote note);
+        void NoteOn(MidiNote note);
+        void Reset();
+    }
+}
