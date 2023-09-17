046f606d765ab97e6a1d45ebf58b5d27c7674df9
ysdy44 <ysdy44@outlook.com>
2023-09-17 14:47:27 +0800

    TrackLayout 3.0.0

--- Virtual Piano.Midi.Core/Structs/TrackLayout.cs
+++ Virtual Piano.Midi.Core/Structs/TrackLayout.cs
@@ -6,7 +6,7 @@ namespace Virtual_Piano.Midi.Core
     {
         //@Const
         public const int Scaling = 4;
-        public const int ItemSize = 21;
+        public const int ItemSize = 66;
         public const int ItemSizeHalf = ItemSize / 2;
 
         public const int Step = 120;
@@ -16,26 +16,18 @@ namespace Virtual_Piano.Midi.Core
 
         public readonly int Pane;
         public readonly int Head;
-        public readonly int Foot;
+        public int Timerline1 => Head;
+        public Thickness Margin => new Thickness(this.Pane, this.Head, 0, 0);
 
-        public readonly Thickness Margin;
+        public int ExtentHeight => 16 * ItemSize;
+        public int ExtentHeightHead => ExtentHeight + Head;
+        public int ExtentHeightFoot => ExtentHeight + Head;
+        public int ExtentHeightHeadFoot => ExtentHeight + Head;
 
-        public readonly int ExtentHeight;
-        public readonly int ExtentHeightHead;
-        public readonly int ExtentHeightFoot;
-
-        public TrackLayout(int pane, int head, int foot) // 75, 18, 140
+        public TrackLayout(int pane, int head) // 175, 18
         {
             this.Pane = pane;
             this.Head = head;
-            this.Foot = foot;
-
-            this.Margin = new Thickness(pane, head, 0, 0);
-
-            int h = NoteExtensions.NoteCount * TrackLayout.ItemSize;
-            this.ExtentHeight = h;
-            this.ExtentHeightHead = h + head;
-            this.ExtentHeightFoot = h + foot;
         }
     }
 }
