20f41229f648a3d41a8adf37b91e6e06a35dd8f6
ysdy44 <ysdy44@outlook.com>
2023-09-17 14:47:36 +0800

    Add MidiChannel

---
+++ Virtual Piano.Midi/Enums3/MidiChannel.cs
@@ -0,0 +1,22 @@
+﻿namespace Virtual_Piano.Midi
+{
+    public enum MidiChannel
+    {
+        Channel0,
+        Channel1,
+        Channel2,
+        Channel3,
+        Channel4,
+        Channel5,
+        Channel6,
+        Channel7,
+        Channel8,
+        Channel9Percussion,
+        Channel10,
+        Channel11,
+        Channel12,
+        Channel13,
+        Channel14,
+        Channel15,
+    }
+}
