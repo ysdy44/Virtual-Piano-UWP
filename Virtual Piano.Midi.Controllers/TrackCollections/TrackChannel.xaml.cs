3d376b98f2b31a546fea456739ae9ebb7bcff327
ysdy44 <ysdy44@outlook.com>
2023-09-17 14:37:23 +0800

    Add TrackChannel

---
+++ Virtual Piano.Midi.Controllers/TrackCollections/TrackChannel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    [ContentProperty(Name = nameof(Text))]
+    public sealed partial class TrackChannel : UserControl
+    {
+        public string Label
+        {
+            get => this.LabelTextBlock.Text;
+            set => this.LabelTextBlock.Text = value;
+        }
+        public string Text
+        {
+            get => this.TextBlock.Text;
+            set => this.TextBlock.Text = value;
+        }
+        public object CommandParameter
+        {
+            get => this.Button.CommandParameter;
+            set => this.Button.CommandParameter = value;
+        }
+        public ICommand Command 
+        {
+            get => this.Button.Command;
+            set => this.Button.Command = value;
+        }
+
+        public TrackChannel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
