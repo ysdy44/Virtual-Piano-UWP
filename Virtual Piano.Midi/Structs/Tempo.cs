cf1727168434367a8da9198c836ee28d06e2594e
ysdy44 <ysdy44@outlook.com>
2023-09-24 17:38:27 +0800

    Tempo 3.0.0

--- Virtual Piano.Midi/Structs/Tempo.cs
+++ Virtual Piano.Midi/Structs/Tempo.cs
@@ -8,12 +8,25 @@ namespace Virtual_Piano.Midi
         public readonly int MicrosecondsPerQuarterNote;
         public readonly TimeSpan Delay;
 
-        public Tempo(int bpm = 60)
+        public Tempo(int bpm = 120)
         {
-            this.Bpm = bpm;
-            this.MicrosecondsPerQuarterNote = 60 * 1000 * 1000 / bpm;
-            this.Delay = TimeSpan.FromMilliseconds(60 * 1000 / bpm / 4);
+            if (bpm == 0)
+            {
+                this.Bpm = 120;
+                this.MicrosecondsPerQuarterNote = 500000;
+                this.Delay = TimeSpan.FromMilliseconds(125);
+            }
+            else
+            {
+                this.Bpm = bpm;
+                this.MicrosecondsPerQuarterNote = 60 * 1000 * 1000 / bpm;
+                this.Delay = TimeSpan.FromMilliseconds(60 * 1000 / bpm / 4);
+            }
         }
+
+        public int GetPosition(long position) => (int)(1d * this.Bpm / 120d * position);
+
+        public int GetDuration(int duration) => (int)(120d * duration / this.Bpm);
 
         public override string ToString() => $"Tempo {this.Bpm}bpm ({this.MicrosecondsPerQuarterNote})";
     }
