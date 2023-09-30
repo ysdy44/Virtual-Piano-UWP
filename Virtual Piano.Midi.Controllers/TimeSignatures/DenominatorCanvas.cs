494ce39b87124eda551ade9a0a1c22a11a6c0e14
ysdy44 <ysdy44@outlook.com>
2023-09-30 20:08:34 +0800

    Add NumeratorCanvas and DenominatorCanvas

---
+++ Virtual Piano.Midi.Controllers/TimeSignatures/DenominatorCanvas.cs
@@ -0,0 +1,58 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    public sealed class DenominatorCanvas : Canvas
+    {
+        //@Const
+        public const int Maximum = 12;
+        public const int Minimum = 1;
+        private const int Max = NumeratorCanvas.Maximum * (Maximum - 1);
+        private const int Min = NumeratorCanvas.Minimum * (Minimum - 1);
+
+        public DenominatorCanvas()
+        {
+            for (int i = 0; i < Max; i++)
+            {
+                base.Children.Add(new Line
+                {
+                    Visibility = Visibility.Collapsed
+                });
+            }
+        }
+
+        public void Update(int numerator, int denominator, double length = 400)
+        {
+            double space = length / numerator;
+            double space2 = space / (denominator);
+
+            for (int n = 0; n < numerator; n++)
+            {
+                double y = n * space;
+
+                for (int d = 0; d < (denominator - 1); d++)
+                {
+                    int i = n * (denominator - 1) + d;
+
+                    double x = y + (space2 * (d + 1));
+                    if (base.Children[i] is Line line)
+                    {
+                        line.X1 = x;
+                        line.X2 = x;
+                        line.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+
+            for (int i = numerator * (denominator - 1); i < Max; i++)
+            {
+                if (base.Children[i] is Line line)
+                {
+                    line.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
---
+++ Virtual Piano.Midi.Controllers/TimeSignatures/NumeratorCanvas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    public sealed class NumeratorCanvas : Canvas
+    {
+        //@Const
+        public const int Maximum = 12;
+        public const int Minimum = 1;
+        private const int Min = Minimum;
+        private const int Max = Maximum;
+
+        public NumeratorCanvas()
+        {
+            base.Children.Add(new Line());
+            for (int i = 1; i < Max; i++)
+            {
+                base.Children.Add(new Line
+                {
+                    Visibility = Visibility.Collapsed
+                });
+            }
+            base.Children.Add(new Line());
+        }
+
+        public void Update(int numerator, double length = 400)
+        {
+            double space = length / numerator;
+
+            for (int n = 1; n <= numerator; n++)
+            {
+                double x = n * space;
+                if (base.Children[n] is Line line)
+                {
+                    line.X1 = x;
+                    line.X2 = x;
+                    line.Visibility = Visibility.Visible;
+                }
+            }
+
+            for (int i = numerator + 1; i < Max; i++)
+            {
+                if (base.Children[i] is Line line)
+                {
+                    line.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
