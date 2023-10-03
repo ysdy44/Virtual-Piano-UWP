4a902289453065bd1afd9572ad5e04cc7d775bb2
ysdy44 <ysdy44@outlook.com>
2023-10-03 21:22:15 +0800

    TrackCollection 2.0.0: Supports Single, Multiple Mode

--- Virtual Piano.Midi.Controllers/TrackCollections/TrackCollection.cs
+++ Virtual Piano.Midi.Controllers/TrackCollections/TrackCollection.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using NAudio.Midi;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using Virtual_Piano.Midi.Core;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
@@ -26,27 +28,81 @@ namespace Virtual_Piano.Midi.Controllers
             this.StartAbsoluteTime = events.StartAbsoluteTime;
             this.DeltaTicksPerQuarterNote = events.DeltaTicksPerQuarterNote;
 
-            for (int i = 0; i < events.Tracks; i++)
+            int tracks = events.Tracks;
+
+            switch (tracks)
             {
-                IList<NAudio.Midi.MidiEvent> e = events[i];
-                if (e.Count == 0) continue;
+                case 0:
+                    break;
+                case 1:
+                    IList<NAudio.Midi.MidiEvent> events1 = events.Single();
+                    IDictionary<int, IList<MidiEvent>> dictionary = new Dictionary<int, IList<MidiEvent>>();
 
-                Track track = new Track(e);
-                if (this.Duration < track.Source.Duration) this.Duration = track.Source.Duration;
-                if (this.Tempo < track.Source.Tempo) this.Tempo = track.Source.Tempo;
-                if (this.Numerator < track.Source.Numerator) this.Numerator = track.Source.Numerator;
-                if (this.Denominator < track.Source.Denominator) this.Denominator = track.Source.Denominator;
-                if (this.SharpsFlats < track.Source.SharpsFlats) this.SharpsFlats = track.Source.SharpsFlats;
-                if (this.MajorMinor < track.Source.MajorMinor) this.MajorMinor = track.Source.MajorMinor;
+                    foreach (NAudio.Midi.MidiEvent item in events1)
+                    {
+                        int channel = item.Channel;
+                        if (tracks < channel) tracks = channel;
 
-                ContentControl control = new ContentControl
-                {
-                    Width = track.Source.Duration / TrackLayout.Scaling,
-                    Height = TrackLayout.ItemSize,
-                    Content = track
-                };
-                Canvas.SetTop(control, i * TrackLayout.ItemSize);
-                base.Add(control);
+                        if (dictionary.ContainsKey(channel))
+                            dictionary[channel].Add(item);
+                        else
+                            dictionary.Add(channel, new List<MidiEvent>
+                            {
+                                item
+                            });
+                    }
+
+                    for (int i = 0; i <= tracks; i++)
+                    {
+                        if (dictionary.ContainsKey(i) is false) continue;
+
+                        IList<NAudio.Midi.MidiEvent> events2 = dictionary[i];
+                        if (events2 == null) continue;
+                        if (events2.Count == 0) continue;
+
+                        Track track = new Track(events2);
+                        if (this.Duration < track.Source.Duration) this.Duration = track.Source.Duration;
+                        if (this.Tempo < track.Source.Tempo) this.Tempo = track.Source.Tempo;
+                        if (this.Numerator < track.Source.Numerator) this.Numerator = track.Source.Numerator;
+                        if (this.Denominator < track.Source.Denominator) this.Denominator = track.Source.Denominator;
+                        if (this.SharpsFlats < track.Source.SharpsFlats) this.SharpsFlats = track.Source.SharpsFlats;
+                        if (this.MajorMinor < track.Source.MajorMinor) this.MajorMinor = track.Source.MajorMinor;
+
+                        ContentControl control = new ContentControl
+                        {
+                            Width = track.Source.Duration / TrackLayout.Scaling,
+                            Height = TrackLayout.ItemSize,
+                            Content = track
+                        };
+                        Canvas.SetTop(control, i * TrackLayout.ItemSize);
+                        base.Add(control);
+                    }
+                    break;
+                default:
+                    for (int i = 0; i < tracks; i++)
+                    {
+                        IList<NAudio.Midi.MidiEvent> events3 = events[i];
+                        if (events3 == null) continue;
+                        if (events3.Count == 0) continue;
+
+                        Track track = new Track(events3);
+                        if (this.Duration < track.Source.Duration) this.Duration = track.Source.Duration;
+                        if (this.Tempo < track.Source.Tempo) this.Tempo = track.Source.Tempo;
+                        if (this.Numerator < track.Source.Numerator) this.Numerator = track.Source.Numerator;
+                        if (this.Denominator < track.Source.Denominator) this.Denominator = track.Source.Denominator;
+                        if (this.SharpsFlats < track.Source.SharpsFlats) this.SharpsFlats = track.Source.SharpsFlats;
+                        if (this.MajorMinor < track.Source.MajorMinor) this.MajorMinor = track.Source.MajorMinor;
+
+                        ContentControl control = new ContentControl
+                        {
+                            Width = track.Source.Duration / TrackLayout.Scaling,
+                            Height = TrackLayout.ItemSize,
+                            Content = track
+                        };
+                        Canvas.SetTop(control, i * TrackLayout.ItemSize);
+                        base.Add(control);
+                    }
+                    break;
             }
         }
     }
