87d00c4e394cbbbb228b710ad4e9ead2e5ca7a19
ysdy44 <ysdy44@outlook.com>
2023-10-03 16:19:58 +0800

    Add RadarGraphPage

---
+++ Virtual Piano.TestApp/RadarGraphPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.TestApp
+{
+    public sealed partial class RadarGraphPage : Page
+    {
+        public RadarGraphPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
