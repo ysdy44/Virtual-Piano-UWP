669ee966d780dfa2e9fa6e1fe93fd34d0d25e15b
ysdy44 <ysdy44@outlook.com>
2023-10-04 23:05:35 +0800

    Add ITrack

--- Virtual Piano.Midi.Core/Interfaces/ITrackNotePanel.cs
+++ Virtual Piano.Midi.Core/Interfaces/ITrackNotePanel.cs
@@ -6,46 +6,6 @@ using Windows.UI.Xaml.Media;
 
 namespace Virtual_Piano.Midi.Core
 {
-    public interface ITrack
-    {
-        //@Delegate
-        event DragStartedEventHandler DragStarted;
-        event DragDeltaEventHandler DragDelta;
-        event DragCompletedEventHandler DragCompleted;
-
-        // Container
-        int ViewportWidth { get; }
-        int ViewportHeight { get; }
-
-        // Content
-        int HorizontalOffset { get; }
-        int VerticalOffset { get; }
-
-        int Index { get; set; }
-
-        // UI
-        object ItemsSource { get; set; }
-
-        // Timeline
-        int Position { get; }
-
-        TimeSignature TimeSignature { get; }
-        TimeSignatureTicks Ticks { get; }
-
-        void Init(TimeSignature timeSignature, TimeSignatureTicks ticks);
-
-        void ChangeDuration(long duration);
-        void ChangePosition(int position, bool scrollNext, bool scrollPrevious);
-        void Stop();
-        void ChangePositionUI(int positionUI);
-    }
-
-    public interface ITrackPanel : ITrack
-    {
-        //@Delegate
-        event EventHandler<int> ItemClick;
-    }
-
     public interface ITrackNotePanel : ITrack
     {
         //@Delegate
---
+++ Virtual Piano.Midi.Core/Interfaces/ITrackPanel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Virtual_Piano.Midi.Core
+{
+    public interface ITrackPanel : ITrack
+    {
+        //@Delegate
+        event EventHandler<int> ItemClick;
+    }
+}
