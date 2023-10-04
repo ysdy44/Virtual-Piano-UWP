1c3ccdfde6b90b5e0d41e81ea1f7a6e60c137820
ysdy44 <ysdy44@outlook.com>
2023-10-04 21:32:31 +0800

    Add StringExtensions

---
+++ Virtual Piano/Strings/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Virtual_Piano.Midi;
+
+namespace Virtual_Piano.Strings
+{
+    public static class StringExtensions
+    {
+        public static string GetString(this UIType type)
+        {
+            return App.Resource.GetString($"{type}");
+        }
+
+        public static string GetString(this MidiProgramGroup group)
+        {
+            return App.Resource.GetString($"{group}");
+        }
+
+        public static string GetString(this MidiProgram program)
+        {
+            return App.Resource.GetString($"{program}");
+        }
+
+        public static string GetString(this MidiPercussionNote note)
+        {
+            return App.Resource.GetString($"{note}");
+        }
+    }
+}
