762a848a0f59f15b320e0638a3172199fead632b
ysdy44 <ysdy44@outlook.com>
2023-10-09 13:31:40 +0800

    TimeSignatureTicks Add BeatsPerBar

--- Virtual Piano.Midi/Structs/TimeSignatureTicks.cs
+++ Virtual Piano.Midi/Structs/TimeSignatureTicks.cs
@@ -4,17 +4,25 @@ namespace Virtual_Piano.Midi
 {
     public readonly struct TimeSignatureTicks
     {
-        //public readonly int BeatsPerBar; // 4
-     
+        public readonly int BeatsPerBar; // 4
+
         public readonly int TicksPerBar; // 480
         public readonly int TicksPerBeat; // 120
 
         public TimeSignatureTicks(TimeSignature timeSignature, Tempo tempo)
         {
-            //this.BeatsPerBar = timeSignature.Numerator;
-         
+            this.BeatsPerBar = timeSignature.Numerator;
+
             this.TicksPerBar = (timeSignature.Numerator * tempo.TicksPerQuarterNote * 4) / (timeSignature.Denominator);
             this.TicksPerBeat = this.TicksPerBar / timeSignature.Numerator;
+        }
+
+        public TimeSignatureTicks(TimeSignatureTicks oldTicks, TimeSignature oldTimeSignature, TimeSignature timeSignature)
+        {
+            this.BeatsPerBar = timeSignature.Numerator;
+
+            this.TicksPerBar = oldTicks.TicksPerBar * timeSignature.Numerator / oldTimeSignature.Numerator;
+            this.TicksPerBeat = oldTicks.TicksPerBeat;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -28,5 +36,7 @@ namespace Virtual_Piano.Midi
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string ToMBT(long ticks) => $"{this.ToBar(ticks)}:{this.ToBeat(ticks)}:{this.ToTick(ticks)}";
+
+        public override string ToString() => $"Ticks {this.TicksPerBeat}ms/Beat";
     }
 }
