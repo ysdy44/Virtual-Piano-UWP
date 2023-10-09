ca79bb485c6c7fcd7500f5abe700b593198a54d9
ysdy44 <ysdy44@outlook.com>
2023-10-09 13:28:56 +0800

    TempoDuration Add Duration

--- Virtual Piano.Midi/Structs/TempoDuration.cs
+++ Virtual Piano.Midi/Structs/TempoDuration.cs
@@ -5,11 +5,13 @@ namespace Virtual_Piano.Midi
 {
     public readonly struct TempoDuration
     {
+        public readonly long Duration;
         public readonly long DurationMilliseconds;
         private readonly double Percent;
 
         public TempoDuration(Tempo tempo, long duration = 1000 * 60)
         {
+            this.Duration = duration;
             this.DurationMilliseconds = tempo.ToMilliseconds(duration);
             this.Percent = tempo.ToTicksPercent(duration);
         }
