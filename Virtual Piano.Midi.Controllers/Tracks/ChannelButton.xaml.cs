4a1a43a188cdd6310f9ee0bba510dd87f609f799
ysdy44 <ysdy44@outlook.com>
2023-10-31 08:47:02 +0800

    Add ChannelPanel and ChannelButton

---
+++ Virtual Piano.Midi.Controllers/Tracks/ChannelButton.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Virtual_Piano.Midi.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    [ContentProperty(Name = nameof(Text))]
+    public sealed partial class ChannelButton : UserControl, IChannelButton
+    {
+        //@Converter
+        private double OpacityConverter(bool? value) => value is true ? 0.2 : 0.0;
+
+        public byte Channel { get; }
+
+        public string Label { get => this.LabelTextBlock.Text; set => this.LabelTextBlock.Text = value; }
+        public string Text { get => this.TextBlock.Text; set => this.TextBlock.Text = value; }
+
+        public bool? IsMute { get => this.MuteButton.IsChecked; set => this.MuteButton.IsChecked = value; }
+        public bool? IsSolo { get => this.SoloButton.IsChecked; set => this.SoloButton.IsChecked = value; }
+
+        readonly MidiChannelMessage ProgramChange;
+        readonly MidiChannelMessage Edit;
+
+        readonly MidiChannelMessage UnMute;
+        readonly MidiChannelMessage Mute;
+
+        readonly MidiChannelMessage UnSolo;
+        readonly MidiChannelMessage Solo;
+
+        readonly MidiChannelMessage UnRecord;
+        readonly MidiChannelMessage Record;
+
+        public ChannelButton(byte channel)
+        {
+            this.InitializeComponent();
+            this.Channel = channel;
+
+            this.ProgramChange = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.ProgramChange,
+                Channel = channel
+            };
+            this.Edit = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.Edit,
+                Channel = channel
+            };
+
+            this.UnMute = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.UnMute,
+                Channel = channel
+            };
+            this.Mute = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.Mute,
+                Channel = channel
+            };
+
+            this.UnSolo = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.UnSolo,
+                Channel = channel
+            };
+            this.Solo = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.Solo,
+                Channel = channel
+            };
+
+            this.UnRecord = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.UnRecord,
+                Channel = channel
+            };
+            this.Record = new MidiChannelMessage
+            {
+                Type = MidiChannelMessageType.Record,
+                Channel = channel
+            };
+
+            this.Button.Click += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.ProgramChange);
+                }
+            };
+            this.EditButton.Click += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.Edit);
+                }
+            };
+
+            this.MuteButton.Unchecked += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.UnMute);
+                }
+            };
+            this.MuteButton.Checked += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.Mute);
+                }
+            };
+
+            this.SoloButton.Unchecked += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.UnSolo);
+                }
+            };
+            this.SoloButton.Checked += (s, e) =>
+            {
+                if (base.Parent is IChannelPanel item)
+                {
+                    item.OnClick(this.Solo);
+                }
+            };
+        }
+    }
+}
---
+++ Virtual Piano.Midi.Controllers/Tracks/ChannelPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Windows.Input;
+using Virtual_Piano.Midi.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Midi.Controllers
+{
+    public sealed class ChannelPanel : StackPanel, IChannelPanel
+    {
+        //@Command
+        public ICommand Command { get; set; }
+
+        public ChannelPanel()
+        {
+            foreach (MidiChannel item in System.Enum.GetValues(typeof(MidiChannel)).Cast<MidiChannel>())
+            {
+                byte i = (byte)item;
+                base.Children.Add(new ChannelButton(i)
+                {
+                    Height = TrackLayout.ItemSize,
+                    Label = $"{i}",
+                });
+            }
+        }
+
+        public void OnClick(MidiChannelMessage message) => this.Command?.Execute(message); // Command
+
+        public void Demute()
+        {
+            foreach (IChannelButton item in base.Children)
+            {
+                item.IsMute = false;
+            }
+        }
+
+        public void Desolo()
+        {
+            foreach (IChannelButton item in base.Children)
+            {
+                item.IsSolo = false;
+            }
+        }
+
+        public void Desolo(int channel)
+        {
+            for (int i = 0; i < base.Children.Count; i++)
+            {
+                if (i == channel) continue;
+
+                IChannelButton item = (IChannelButton)base.Children[i];
+                item.IsSolo = false;
+            }
+        }
+    }
+}
