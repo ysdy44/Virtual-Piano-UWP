8776e20c8a8db3d479e07514b719788bb96f596e
ysdy44 <ysdy44@outlook.com>
2023-10-31 08:36:32 +0800

    Add IChannelPanel and IChannelButton

---
+++ Virtual Piano.Midi.Core/Interfaces.Buttons/IChannelButton.cs
@@ -0,0 +1,13 @@
+﻿namespace Virtual_Piano.Midi.Core
+{
+    public interface IChannelButton
+    {
+        byte Channel { get; }
+
+        string Label { get; set; }
+        string Text { get; set; }
+
+        bool? IsMute { get; set; }
+        bool? IsSolo { get; set; }
+    }
+}
---
+++ Virtual Piano.Midi.Core/Interfaces.Panels/IChannelPanel.cs
@@ -0,0 +1,11 @@
+﻿namespace Virtual_Piano.Midi.Core
+{
+    public interface IChannelPanel
+    {
+        void OnClick(MidiChannelMessage message);
+
+        void Demute();
+        void Desolo();
+        void Desolo(int channel);
+    }
+}
