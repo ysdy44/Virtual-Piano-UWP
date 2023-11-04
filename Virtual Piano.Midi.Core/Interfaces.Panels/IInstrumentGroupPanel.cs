bfa645c99854e93a1e86004beaf752e807911e3c
ysdy44 <ysdy44@outlook.com>
2023-11-04 23:05:42 +0800

    Delete InstrumentGroupPanel

--- Virtual Piano.Midi.Core/Interfaces.Panels/IInstrumentGroupPanel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Virtual_Piano.Midi.Core
-{
-    public interface IInstrumentGroupPanel : IInstrumentPanel
-    {
-        void Add(MidiProgramGroup group);
-        void Remove(MidiProgramGroup group);
-    }
-}
