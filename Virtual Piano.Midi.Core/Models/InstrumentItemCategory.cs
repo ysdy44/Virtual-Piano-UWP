ff6a7b3d9d2b4cf7876a18bf405c54074ffb7c53
ysdy44 <ysdy44@outlook.com>
2023-11-05 13:03:54 +0800

    File Move: Midi.Core\Models\InstrumentItem -> Controls\InstrumentItem

--- Virtual Piano.Midi.Core/Models/InstrumentItemCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Virtual_Piano.Midi.Core
-{
-    public class InstrumentItemCategory
-    {
-        public MidiProgramGroup Group;
-        public string Text => this.ToString();
-        public override string ToString() => MidiProgramFactory.Emoji[this.Group];
-    }
-}
--- Virtual Piano/Controls/InstrumentItem.cs
+++ Virtual Piano/Controls/InstrumentItem.cs
@@ -1,4 +1,5 @@
 ﻿using Virtual_Piano.Midi;
+using Virtual_Piano.Strings;
 using Windows.UI.Xaml.Controls;
 
 namespace Virtual_Piano.Controls
@@ -8,7 +9,7 @@ namespace Virtual_Piano.Controls
         public MidiProgram Key
         {
             get => default;
-            set => base.Content = $"{(int)value}. {value.ToString()}";
+            set => base.Content = $"{(int)value}. {value.GetString()}";
         }
     }
 }
