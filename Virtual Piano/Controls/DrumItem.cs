2b48f3dd7ec9a8eac860c6fb38a8b3e587f37127
ysdy44 <ysdy44@outlook.com>
2023-11-05 22:29:06 +0800

    Add DrumItem

---
+++ Virtual Piano/Controls/DrumItem.cs
@@ -0,0 +1,15 @@
+﻿using Virtual_Piano.Midi;
+using Virtual_Piano.Strings;
+using Windows.UI.Xaml.Controls;
+
+namespace Virtual_Piano.Controls
+{
+    public sealed class DrumItem : ContentPresenter
+    {
+        public MidiPercussionProgram Key
+        {
+            get => default;
+            set => base.Content = $"{(int)value}. {value.GetString()}";
+        }
+    }
+}
